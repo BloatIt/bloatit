66fd6378b6d3e9b37fa3ba12c885b80275d730f8
Thomas Guyard <thomas.guyard@gmail.com>
2010-10-25 22:43:47 +0200

    Realy add test on description

---
+++ src/test/java/com/bloatit/model/data/DescriptionTranslatableTest.java
@@ -0,0 +1,61 @@
+package com.bloatit.model.data;
+
+import java.util.Locale;
+
+import com.bloatit.model.data.util.SessionManger;
+
+import junit.framework.TestCase;
+
+public class DescriptionTranslatableTest extends TestCase {
+	private Member yo;
+	private Member tom;
+	private Member fred;
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		SessionManger.reCreateSessionFactory();
+		SessionManger.beginWorkUnit();
+		{
+			tom = Member.createAndPersist("Thomas", "password", "tom@gmail.com");
+			tom.setFirstname("Thomas");
+			tom.setLastname("Guyard");
+			SessionManger.flush();
+		}
+		{
+			fred = Member.createAndPersist("Fred", "other", "fred@gmail.com");
+			fred.setFirstname("Frédéric");
+			fred.setLastname("Bertolus");
+			SessionManger.flush();
+		}
+		{
+			yo = Member.createAndPersist("Yo", "plop", "yo@gmail.com");
+			yo.setFirstname("Yoann");
+			yo.setLastname("Plénet");
+			SessionManger.flush();
+
+			Group.createAndPersiste("Other", yo, Group.Right.PUBLIC).addMember(yo, false);
+			Group.createAndPersiste("myGroup", yo, Group.Right.PUBLIC).addMember(yo, false);
+			Group.createAndPersiste("b219", yo, Group.Right.PRIVATE).addMember(yo, true);
+		}
+
+		SessionManger.endWorkUnitAndFlush();
+	}
+
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        if (SessionManger.getSessionFactory().getCurrentSession().getTransaction().isActive()) {
+            SessionManger.endWorkUnitAndFlush();
+        }
+        SessionManger.getSessionFactory().close();
+    }
+    public void testCreateDescritpion() {
+        SessionManger.beginWorkUnit();
+
+        Description description = Description.createAndPersist(yo, new Locale("fr"), "title", "description");
+
+        SessionManger.endWorkUnitAndFlush();
+
+    }
+
+	
+}
