426819e21f0e2962302cab972fa1615cfc8aee62
Thomas Guyard <thomas.guyard@gmail.com>
2010-11-01 18:10:39 +0100

    create the JoinGroupDemand class in framework package

---
+++ src/main/java/com/bloatit/framework/JoinGroupDemand.java
@@ -0,0 +1,26 @@
+package com.bloatit.framework;
+
+import com.bloatit.model.data.DaoJoinGroupDemand;
+
+public class JoinGroupDemand extends Identifiable {
+
+    private DaoJoinGroupDemand dao;
+    
+    public Member getSender() {
+        return Member.create(dao.getSender());
+    }
+
+    public Member getReciever() {
+        return Member.create(dao.getReciever());
+    }
+
+    public Group getGroup() {
+        return Group.create(dao.getGroup());
+    }
+
+    @Override
+    public int getId() {
+        return dao.getId();
+    }
+
+}
