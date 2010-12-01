b4a4e1bac2cd7b2892c326651866c5c45ad306e2
Thomas Guyard <thomas.guyard@gmail.com>
2010-12-01 23:10:20 +0100

    fix create method

--- src/main/java/com/bloatit/framework/JoinGroupInvitation.java
+++ src/main/java/com/bloatit/framework/JoinGroupInvitation.java
@@ -5,6 +5,17 @@ import com.bloatit.model.data.DaoJoinGroupInvitation;
 public class JoinGroupInvitation extends Identifiable {
 
     private DaoJoinGroupInvitation dao;
+    
+    private JoinGroupInvitation(DaoJoinGroupInvitation dao) {
+        this.dao = dao;
+    }
+
+    public static JoinGroupInvitation create(DaoJoinGroupInvitation dao){
+        if ( dao != null){
+            return new JoinGroupInvitation(dao);
+        }
+        return null;
+    }
 
     public Member getSender() {
         return Member.create(dao.getSender());
