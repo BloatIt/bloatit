0a1bc76adfe24c5762c48d9ad07835a8dda0c78d
Yoann Plénet <yplenet@gmail.com>
2010-12-10 12:04:32 +0100

    Fixing tom's mistakes

--- src/main/java/test/pages/master/TopBar.java
+++ src/main/java/test/pages/master/TopBar.java
@@ -1,14 +1,14 @@
 package test.pages.master;
 
 import test.Context;
-import test.HtmlElement;
-import test.pages.components.HtmlBlock;
 
 import com.bloatit.web.actions.LogoutAction;
 import com.bloatit.web.htmlrenderer.HtmlTools;
 import com.bloatit.web.pages.LoginPage;
 import com.bloatit.web.pages.MyAccountPage;
 import com.bloatit.web.server.Session;
+import test.html.components.standard.HtmlBlock;
+import test.html.components.standard.HtmlGenericElement;
 
 public class TopBar extends HtmlBlock {
 
@@ -25,14 +25,13 @@ public class TopBar extends HtmlBlock {
                     + "<span class=\"karma\">" + karma + "</span>";
             final String logoutLink = HtmlTools.generateLink(session, session.tr("Logout"), new LogoutAction(session));
 
-            add(new HtmlElement("span").setClass("top_bar_component").addText(memberLink));
-            add(new HtmlElement("span").setClass("top_bar_component").addText(logoutLink));
+            add(new HtmlGenericElement("span").setClass("top_bar_component").addText(memberLink));
+            add(new HtmlGenericElement("span").setClass("top_bar_component").addText(logoutLink));
 
         } else {
-            add(new HtmlElement("span").setClass("top_bar_component").addText(HtmlTools.generateLink(session,
+            add(new HtmlGenericElement("span").setClass("top_bar_component").addText(HtmlTools.generateLink(session,
                     session.tr("Login / Signup"),
                     new LoginPage(session))));
-
         }
     }
 
