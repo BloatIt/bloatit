a9d865826472a774b0c422f406228d54223cc85e
Frédéric Bertolus <fred.bertolus@gmail.com>
2010-12-14 17:43:24 +0100

    Change the display name for demand page from "demandpage" to "demand"

--- main/src/main/java/com/bloatit/web/html/pages/demand/DemandPage.java
+++ main/src/main/java/com/bloatit/web/html/pages/demand/DemandPage.java
@@ -29,10 +29,12 @@ import com.bloatit.web.html.pages.master.Page;
 import com.bloatit.web.server.Context;
 import com.bloatit.web.utils.Message.Level;
 import com.bloatit.web.utils.annotations.PageComponent;
+import com.bloatit.web.utils.annotations.PageName;
 import com.bloatit.web.utils.annotations.RequestParam;
 import com.bloatit.web.utils.annotations.RequestParam.Role;
 import com.bloatit.web.utils.url.Request;
 
+@PageName("demand")
 public class DemandPage extends Page {
 
     @RequestParam(name = "id", level = Level.ERROR)
