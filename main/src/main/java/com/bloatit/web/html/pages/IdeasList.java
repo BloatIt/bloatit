8027b32d83337da52a468cf06c2afd0d7a2b5616
Frédéric Bertolus <fred.bertolus@gmail.com>
2010-12-17 19:08:05 +0100

    improve ideas list design

--- main/src/main/java/com/bloatit/web/html/pages/IdeasList.java
+++ main/src/main/java/com/bloatit/web/html/pages/IdeasList.java
@@ -39,6 +39,8 @@ import com.bloatit.web.html.components.standard.HtmlTitleBlock;
 import com.bloatit.web.html.pages.master.Page;
 import com.bloatit.web.utils.url.IdeaPageUrl;
 import com.bloatit.web.utils.url.IdeasListUrl;
+import java.util.Random;
+import javax.rmi.CORBA.Util;
 
 @ParamContainer("ideas/list")
 public class IdeasList extends Page {
@@ -92,33 +94,98 @@ public class IdeasList extends Page {
         @Override
         public HtmlNode generate(final Demand idea) {
             this.demand = idea;
-            HtmlLink link = new IdeaPageUrl(demand).getHtmlLink(generateContent());
-            link.setCssClass("idea_link");
-            return new HtmlListItem(link);
+            
+            
+            return generateContent();
         }
 
         private HtmlNode generateContent() {
 
             final HtmlDiv ideaBlock = new HtmlDiv("idea_summary");
             {
+
+
+                final HtmlDiv ideaLinkBlock = new HtmlDiv();
+
+                HtmlLink link = new IdeaPageUrl(demand).getHtmlLink(ideaLinkBlock);
+                link.setCssClass("idea_link");
+                ideaBlock.add(link);
+
                 final HtmlDiv leftBlock = new HtmlDiv("idea_summary_left");
                 {
-                    leftBlock.add(new HtmlImage(new Image("/resources/img/tux_mini.png", Image.ImageType.DISTANT)));
-                    leftBlock.add(new HtmlParagraph("" + demand.getPopularity()));
+
+                    final HtmlDiv karmaBlock = new HtmlDiv("idea_karma");
+                    //karmaBlock.add(new HtmlParagraph("" + demand.getPopularity()));
+                    karmaBlock.add(new HtmlParagraph("" + Math.round(Math.random() * 150)));
+
+                    leftBlock.add(karmaBlock);
+
+
                 }
-                ideaBlock.add(leftBlock);
+                ideaLinkBlock.add(leftBlock);
+
+                final HtmlDiv centerBlock = new HtmlDiv("idea_summary_center");
+                {
+
+
+                    final HtmlTitleBlock ideaTitle = new HtmlTitleBlock("Correction de bug - VLC", 3);
+                    {
+                        ideaTitle.add(new HtmlParagraph(demand.getTitle()));
+                        final HtmlProgressBar progressBar = new HtmlProgressBar((float) (Math.random() * 100f));
+                        ideaTitle.add(progressBar);
+                    }
+                    centerBlock.add(ideaTitle);
+
+
+
+
+                }
+                ideaLinkBlock.add(centerBlock);
 
                 final HtmlDiv rightBlock = new HtmlDiv("idea_summary_right");
                 {
-                    final HtmlTitleBlock ideaTitle = new HtmlTitleBlock("Logiciel - VLC", 3);
-                    {
-                        ideaTitle.add(new HtmlParagraph(demand.getTitle()));
-                        final HtmlProgressBar progressBar = new HtmlProgressBar(0.3f);
-                        ideaTitle.add(progressBar);
+
+                    int imageCount = 7;
+                    int imageIndex = new Random().nextInt(7);
+                    String image = "";
+                    switch (imageIndex) {
+
+                        case 0:
+                            image = "idea";
+                            break;
+                        case 1:
+                            image = "kde";
+                            break;
+                        case 2:
+                            image = "gnome";
+                            break;
+                        case 3:
+                            image = "enlightenment";
+                            break;
+                        case 4:
+                            image = "lxde";
+                            break;
+                        case 5:
+                            image = "ratpoison";
+                            break;
+                        case 6:
+                            if(Math.random() < 0.5) {
+                                image = "tux_mini";
+                            } else {
+                                image = "ratpoison";
+                            }
+                            break;
+
                     }
-                    rightBlock.add(ideaTitle);
+
+
+
+                    rightBlock.add(new HtmlImage(new Image("/resources/img/" + image + ".png", Image.ImageType.DISTANT)));
+
+
+
                 }
-                ideaBlock.add(rightBlock);
+                ideaLinkBlock.add(rightBlock);
             }
             return ideaBlock;
         }
