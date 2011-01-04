6bf2bf54c214b0fea1f403292723289d3a768e7f
Yoann Plénet <yplenet@gmail.com>
2011-01-04 20:30:05 +0100

    FIX forgot files

---
+++ main/src/main/java/com/bloatit/web/utils/i18n/CurrencyLocale.java
@@ -0,0 +1,169 @@
+/*
+ * Copyright (C) 2010 BloatIt.
+ *
+ * This file is part of BloatIt.
+ *
+ * BloatIt is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BloatIt is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with BloatIt. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.bloatit.web.utils.i18n;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+import java.util.Currency;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import com.bloatit.common.FatalErrorException;
+
+/**
+ * Class to handle localization of money amounts.
+ */
+public class CurrencyLocale {
+    private final static String DEFAULT_CURRENCY = "EURO";
+    private final static String DEFAULT_CURRENCY_SYMBOL = "€";
+    private final static int INTERNAL_PRECISION = 6;
+    private final static String RATES_PATH = "../locales/rates";
+    private final static RoundingMode ROUNDING_MODE = RoundingMode.HALF_DOWN;
+    private final static int DISPLAY_PRECISION = 2;
+    
+    private final Locale targetLocale;
+    private final BigDecimal euroAmount;
+    private final Currency currency;
+
+    private static Date lastParse;
+    private static Map<Currency, BigDecimal> currencies = new HashMap<Currency, BigDecimal>();
+
+
+    public CurrencyLocale(BigDecimal euroAmount, Locale targetLocale) {
+        this.euroAmount = euroAmount;
+        this.targetLocale = targetLocale;
+        this.currency = Currency.getInstance(targetLocale);
+        parseRate();
+    }
+
+    /**
+     * <p>Converts the euro amount to the locale amount</p>
+     * <p>Conversion will be done with a 7 digits precision and rounding HALF_ELVEN
+     * (meaning it will round to the closest neighbor unless both are equidistant
+     * in which case it will round to the closest even number) which is the
+     * IEEE 754R default</p>
+     * @return the locale amount
+     */
+    public BigDecimal getConvertedAmount() {
+        return euroAmount.multiply(currencies.get(currency),MathContext.DECIMAL32);
+    }
+
+    /**
+     * Finds the symbol used for this money in the given locale
+     * @return the currency symbol
+     */
+    public String getLocaleSymbol() {
+        return currency.getSymbol();
+    }
+
+    /**
+     * Returns the localized version of the amount, i.e. : converted to the locale
+     * money, and with the locale symbol
+     * @return the localized string
+     */
+    public String getLocaleString() {
+        return getConvertedAmount().setScale(DISPLAY_PRECISION, ROUNDING_MODE) + getLocaleSymbol();
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getDefaultString() {
+        return this.euroAmount.toPlainString() + " " + DEFAULT_CURRENCY_SYMBOL;
+    }
+
+    /**
+     * Returns the localized version of the amount, i.e. : converted to the locale
+     * money, and with the locale symbol
+     * @return the localized string
+     */
+    @Override
+    public String toString(){
+        return this.getLocaleString();
+    }
+    
+    /**
+     * Checks wether the target currency is handled
+     * @return <i>true</i> if currency is handled, <i>false</i> otherwise
+     */ 
+    public boolean availableTargetCurrency(){
+    	return currencies.containsKey(currency);
+    }
+    
+    /**
+     * Checks if a currency is handled
+     * @param currency the currency to check
+     * @return <i>true</i> if currency is handled, <i>false</i> otherwise
+     */
+    public static boolean availableCurrency(Locale locale){
+    	return availableCurrency(Currency.getInstance(locale));
+    }
+    
+    /**
+     * Checks if a currency is handled
+     * @param currency the currency to check
+     * @return <i>true</i> if currency is handled, <i>false</i> otherwise
+     */
+    public static boolean availableCurrency(Currency currency){
+    	return currencies.containsKey(currency);
+    }
+
+    /**
+     * <p>Parses the rate file and initializes the currency array</p>
+     * <p>This parsing will occur only if file has been modified since last parse
+     * or if no parse ever occured</p>
+     */
+    private static void parseRate() {
+        BufferedReader br = null;
+        try {
+            File file = new File(RATES_PATH);
+            if (lastParse == null || lastParse.before(new Date(file.lastModified()))) {
+                // Only parse if the file has been updated in the meantime
+                lastParse = new Date();
+
+                br = new BufferedReader(new FileReader(file));
+                while (br.ready()) {
+                    String line = br.readLine();
+                    if (line.charAt(0) != '#') {
+                        String data = line.split("#", 1)[0];
+                        String code = data.split("\t")[0];
+                        BigDecimal value = new BigDecimal(data.split("\t")[1]);
+
+                        currencies.put(Currency.getInstance(code), value);
+                    }
+                }
+            }
+        } catch (IOException ex) {
+            throw new FatalErrorException(ex);
+        } finally {
+            try {
+                br.close();
+            } catch (Exception ex) {
+            }
+        }
+    }
+}
---
+++ main/src/main/java/com/bloatit/web/utils/i18n/DateLocale.java
@@ -0,0 +1,324 @@
+/*
+ * Copyright (C) 2010 BloatIt. This file is part of BloatIt. BloatIt is free software: you
+ * can redistribute it and/or modify it under the terms of the GNU Affero General Public
+ * License as published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version. BloatIt is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details. You should have received a copy of the GNU Affero General
+ * Public License along with BloatIt. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.bloatit.web.utils.i18n;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+
+/**
+ * <p>
+ * A class to handle dates for the front web
+ * </p>
+ * <p>
+ * Encapsulates all operations concerning date parsing
+ * </p>
+ * <p>
+ * Date format is represented by 2 elements : * The locale that gives the order of the
+ * elements (example : fr : dd/mm/yyyy) * The style that determines the length of the date
+ * representation
+ * </p>
+ * <p>
+ * BloatidDate can be used to parse string into dates (usually after a user wrote a date
+ * in a form) or to find the representation of a date for a given locale (usually to
+ * display a date stored in the database)
+ * </p>
+ * <p>
+ * When parsing a string, Parser will <b>NOT</b> use heuristics meaning the input has to
+ * match the pattern, or an exception will be thrown. This comportment allows much better
+ * error detections
+ * </p>
+ * <p>
+ * For obvious reasons, parser will not be able to detect errors involving inversion of
+ * days and month when day is inferior to 13
+ * </p>
+ * <p>
+ * Note that, when constructing a DateLocale from a String, no proactive validity
+ * checking needs to be done, the parser will make sure the string matches the pattern.
+ * Therefore, the methods getPattern need only be used for display purpose, to indicate
+ * the user how he should input the date
+ * </p>
+ * </p>Also note that parser is fairly tolerant to structure of the date. For the US date
+ * 1/20/1990, the following input strings will <b>all</b> give correct result : *
+ * 1/20/1990 * 01/20/1990 * 1/20/90 * 01/20/90</p>
+ * <p>
+ * While tolerant with short day/months/years, parser is not tolerant with incorrect use
+ * of separator. Therefore, patterns using a '/' must be inputed using a '/' to separate
+ * months, days and years. No other symbol (such as '-' or '.') will be valid.
+ * </p>
+ */
+public final class DateLocale {
+
+    private Date javaDate;
+    private String dateString;
+    private final Locale locale;
+    private FormatStyle style = FormatStyle.SHORT;
+    private final DateFormat formatter;
+
+    /**
+     * <p>
+     * Describes the format of the date
+     * </p>
+     * <p>
+     * * SHORT is completely numeric, such as 12.13.52 or 3:30pm * MEDIUM is longer, such
+     * as Jan 12, 1952 * LONG is longer, such as January 12, 1952 or 3:30:32pm * FULL is
+     * pretty completely specified, such as Tuesday, April 12, 1952 AD or 3:30:42pm PST.
+     * </p>
+     */
+    public enum FormatStyle {
+        SHORT, MEDIUM, LONG, FULL
+    }
+
+    /**
+     * <p>
+     * Creates a BloatItDate based on a string and a locale
+     * </p>
+     * <p>
+     * The date must be short format, aka 31/12/2010
+     * </p>
+     * 
+     * @param dateString the String description of the date (in Short format)
+     * @param locale the user locale to determine date order
+     * @throws DateParsingException When dateString doesn't contain a String that matches
+     *         the current user locale
+     */
+    public DateLocale(final String dateString, final Locale locale) throws DateParsingException {
+        this(dateString, locale, FormatStyle.SHORT);
+    }
+
+    /**
+     * <p>
+     * Creates a BloatItDate based on a string and a locale
+     * </p>
+     * 
+     * @param dateString the String description of the date (in Short format)
+     * @param locale the user locale to determine date order
+     * @param style the style of the date (aka SHORT, LONG, MEDIUM or FULL)
+     * @throws DateParsingException When dateString doesn't contain a String that matches
+     *         the current user locale
+     */
+    public DateLocale(final String dateString, final Locale locale, final FormatStyle style) throws DateParsingException {
+        this.dateString = dateString;
+        this.locale = locale;
+        this.style = style;
+        this.formatter = DateFormat.getDateInstance(this.getJavaStyle(), locale);
+        parseDate();
+    }
+
+    /**
+     * <p>
+     * Creates a BloatItDate based on a java Date with a defaut SHORT style
+     * </p>
+     * 
+     * @param javaDate the Date to convert
+     * @param locale the locale for the date
+     */
+    public DateLocale(final Date javaDate, final Locale locale) {
+        this(javaDate, locale, FormatStyle.SHORT);
+    }
+
+    /**
+     * <p>
+     * Creates a BloatItDate based on a java Date
+     * </p>
+     * 
+     * @param javaDate the Date to convert
+     * @param locale the locale for the date
+     * @param style the style of the date
+     */
+    public DateLocale(final Date javaDate, final Locale locale, final FormatStyle style) {
+        this.locale = locale;
+        this.javaDate = javaDate;
+        this.style = style;
+        this.formatter = DateFormat.getTimeInstance(this.getJavaStyle(), locale);
+        this.dateString = formatter.format(javaDate);
+    }
+
+    /**
+     * <p>
+     * Modifies the date.
+     * </p>
+     * <p>
+     * This method is provided for efficiency only. Clean usage should involve creating a
+     * new object for every dates
+     * </p>
+     * 
+     * @param dateString the String representing the new date value
+     * @throws DateParsingException When dateString doesn't contain a String that matches
+     *         the current user locale
+     */
+    public void setDate(final String dateString) throws DateParsingException {
+        try {
+            this.dateString = dateString;
+            parseDate();
+        } catch (final DateParsingException e) {
+            // Leave the object in its previous state
+            throw new DateParsingException();
+        }
+    }
+
+    public void setDate(final Date javaDate) {
+        this.javaDate = javaDate;
+    }
+
+    /**
+     * @return the date as a Java Date object
+     */
+    public Date getJavaDate() {
+        return this.javaDate;
+    }
+
+    /**
+     * Returns the string representation of the Date, short style
+     * 
+     * @return the string representation of the date, using Short style
+     */
+    @Override
+    public String toString() {
+        return this.toString(FormatStyle.SHORT);
+    }
+
+    /**
+     * Converts the date into aString using a given style
+     * 
+     * @param style the style of the output (SHORT, MEDIUM, LONG, FULL)
+     * @return
+     */
+    public String toString(final FormatStyle style) {
+        final DateFormat df = DateFormat.getDateInstance(getJavaStyle(style), this.locale);
+        return df.format(javaDate);
+    }
+
+    /**
+     * Parses the String into a java Date object
+     * 
+     * @throws DateParsingException When this.dateString doesn't contain a String that
+     *         matches the current user locale
+     */
+    private void parseDate() throws DateParsingException {
+        this.formatter.setLenient(false);
+        try {
+            this.javaDate = formatter.parse(this.dateString);
+        } catch (final ParseException e) {
+            throw new DateParsingException();
+        }
+    }
+
+    /**
+     * <p>
+     * Converts this.style into a style usable by the Java DateFormat Object
+     * </p>
+     * 
+     * @return the converted style
+     */
+    private int getJavaStyle() {
+        return getJavaStyle(this.style);
+    }
+
+    /**
+     * <p>
+     * Converts a given style into a style usable by the Java DateFormat Object
+     * </p>
+     * 
+     * @param style the style to convert
+     * @return the converted style
+     */
+    private static int getJavaStyle(final FormatStyle style) {
+        switch (style) {
+        case MEDIUM:
+            return DateFormat.MEDIUM;
+        case LONG:
+            return DateFormat.LONG;
+        case FULL:
+            return DateFormat.FULL;
+        case SHORT:
+        default:
+            return DateFormat.SHORT;
+        }
+    }
+
+    /**
+     * Returns the current pattern used to display the date
+     * 
+     * @return
+     */
+    public String getPattern() {
+        return DateLocale.getPattern(this.locale, this.style);
+    }
+
+    /**
+     * Returns a String representing the short
+     * 
+     * @param locale
+     * @return
+     */
+    static public String getPattern(final Locale locale) {
+        return DateLocale.getPattern(locale, FormatStyle.SHORT);
+    }
+
+    /**
+     * Returns a String representing the pattern for the SHORT variant
+     * 
+     * @param locale
+     * @return
+     */
+    static public String getPattern(final Locale locale, final FormatStyle format) {
+        final SimpleDateFormat sdf = (SimpleDateFormat) DateFormat.getDateInstance(getJavaStyle(format), locale);
+        return cleanPattern(sdf.toPattern());
+    }
+
+    /**
+     * <p>
+     * Make the pattern shiny for user interaction
+     * </p>
+     * <p>
+     * Make sure the pattern is ready to be displayed to the user : * Days and months will
+     * be shown with 2 digits * Year will be shown with 4 digits * Remove all caps
+     * </p>
+     * 
+     * @param pattern the ugly pattern
+     * @return the nice pattern
+     */
+    static private String cleanPattern(final String pattern) {
+        String separator = "";
+        final String[] separatorList = new String[] { "-", "/", "." };
+
+        // Find separator
+        for (final String sep : separatorList) {
+            if (pattern.contains(sep)) {
+                separator = sep;
+                break;
+            }
+        }
+
+        // Build the new string
+        final String[] dateElements = pattern.toLowerCase().split(separator);
+        final StringBuilder outDate = new StringBuilder();
+
+        for (final String element : dateElements) {
+            if (outDate.length() > 0) {
+                outDate.append(separator);
+            }
+
+            if (element.contains("y")) {
+                outDate.append("yyyy");
+            } else if (element.contains("d")) {
+                outDate.append("dd");
+            } else if (element.contains("m")) {
+                outDate.append("mm");
+            }
+        }
+        return outDate.toString();
+    }
+}
