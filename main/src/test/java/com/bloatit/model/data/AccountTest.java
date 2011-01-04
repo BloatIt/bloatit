1d6577281d0d7df2e909411111fe1b34f9ff5e9e
Thomas Guyard <thomas.guyard@gmail.com>
2011-01-04 12:40:57 +0100

    Improve tests on DaoLayer

--- main/src/test/java/com/bloatit/model/data/AccountTest.java
+++ main/src/test/java/com/bloatit/model/data/AccountTest.java
@@ -1,62 +1,49 @@
 package com.bloatit.model.data;
 
-import java.util.Locale;
-
-import junit.framework.TestCase;
-
+import com.bloatit.common.FatalErrorException;
 import com.bloatit.model.data.DaoExternalAccount.AccountType;
-import com.bloatit.model.data.util.SessionManager;
+import com.bloatit.model.data.util.NonOptionalParameterException;
 
-public class AccountTest extends TestCase {
-    private DaoMember yo;
-    private DaoMember tom;
-    private DaoMember fred;
-    private DaoGroup b219;
-
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-        SessionManager.generateTestSessionFactory();
-        SessionManager.beginWorkUnit();
-        {
-            tom = DaoMember.createAndPersist("Thomas", "password", "tom@gmail.com", Locale.FRANCE);
-            tom.setFullname("Thomas Guyard");
-            SessionManager.flush();
-        }
-        {
-            fred = DaoMember.createAndPersist("Fred", "other", "fred@gmail.com", Locale.FRANCE);
-            fred.setFullname("Frédéric Bertolus");
-            SessionManager.flush();
-        }
-        {
-            yo = DaoMember.createAndPersist("Yo", "plop", "yo@gmail.com", Locale.FRANCE);
-            yo.setFullname("Yoann Plénet");
-            SessionManager.flush();
-
-            DaoGroup.createAndPersiste("Other", "plop@plop.com", DaoGroup.Right.PUBLIC).addMember(yo, false);
-            DaoGroup.createAndPersiste("myGroup", "plop1@plop.com", DaoGroup.Right.PUBLIC).addMember(yo, false);
-            (b219 = DaoGroup.createAndPersiste("b219", "plop2@plop.com", DaoGroup.Right.PROTECTED)).addMember(yo, true);
-        }
-
-        SessionManager.endWorkUnitAndFlush();
-    }
-
-    @Override
-    protected void tearDown() throws Exception {
-        super.tearDown();
-        if (SessionManager.getSessionFactory().getCurrentSession().getTransaction().isActive()) {
-            SessionManager.endWorkUnitAndFlush();
-        }
-        SessionManager.getSessionFactory().close();
-    }
+public class AccountTest extends ModelTestUnit {
 
     public void testDaoAccountDaoActor() {
-        SessionManager.beginWorkUnit();
+        DaoMember localTom = DaoMember.getByLogin(tom.getLogin());
+        localTom.setExternalAccount(DaoExternalAccount.createAndPersist(localTom, AccountType.IBAN, "Bank code !"));
 
-        tom.setExternalAccount(DaoExternalAccount.createAndPersist(tom, AccountType.IBAN, "Bank code !"));
-        b219.setExternalAccount(DaoExternalAccount.createAndPersist(b219, AccountType.IBAN, "Bank code !"));
+        DaoGroup localB219 = DaoGroup.getByName(b219.getLogin());
+        localB219.setExternalAccount(DaoExternalAccount.createAndPersist(localB219, AccountType.IBAN, "Bank code !"));
 
-        SessionManager.endWorkUnitAndFlush();
+        try {
+            localTom.setExternalAccount(DaoExternalAccount.createAndPersist(null, AccountType.IBAN, "Bank code !"));
+            fail();
+        } catch (NonOptionalParameterException e) {
+            assertTrue(true);
+        }
+        try {
+            localTom.setExternalAccount(DaoExternalAccount.createAndPersist(localTom, null, "Bank code !"));
+            fail();
+        } catch (NonOptionalParameterException e) {
+            assertTrue(true);
+        }
+        try {
+            localTom.setExternalAccount(DaoExternalAccount.createAndPersist(localTom, AccountType.IBAN, null));
+            fail();
+        } catch (NonOptionalParameterException e) {
+            assertTrue(true);
+        }
+        try {
+            localTom.setExternalAccount(DaoExternalAccount.createAndPersist(localTom, AccountType.IBAN, ""));
+            fail();
+        } catch (NonOptionalParameterException e) {
+            assertTrue(true);
+        }
+
+        try {
+            localTom.setExternalAccount(DaoExternalAccount.createAndPersist(localTom, AccountType.IBAN, ""));
+            fail();
+        } catch (FatalErrorException e) {
+            assertTrue(true);
+        }
     }
 
 }
