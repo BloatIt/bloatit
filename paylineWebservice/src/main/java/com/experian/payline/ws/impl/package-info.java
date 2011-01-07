730f1a056ba93303beaa6cd7578ba5d6beaa7963
Thomas Guyard <thomas.guyard@gmail.com>
2011-01-07 20:10:50 +0100

    remove generated files

--- paylineWebservice/src/main/java/com/experian/payline/ws/impl/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://impl.ws.payline.experian.com", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.experian.payline.ws.impl;
--- paylineWebservice/src/main/java/com/experian/payline/ws/obj/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://obj.ws.payline.experian.com", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.experian.payline.ws.obj;
