466d52f7963494f1fcf5c736662833dc938c4170
Thomas Guyard <thomas.guyard@gmail.com>
2011-01-07 11:48:04 +0100

    add a url binder for non generated urls

---
+++ main/src/main/java/com/bloatit/web/utils/url/UrlStringBinder.java
@@ -0,0 +1,37 @@
+package com.bloatit.web.utils.url;
+
+import com.bloatit.web.exceptions.RedirectException;
+import com.bloatit.web.server.Linkable;
+
+public class UrlStringBinder extends Url {
+
+    private final String url;
+
+    public UrlStringBinder(String url) {
+        super("");
+        this.url = url;
+    }
+
+    @Override
+    public Linkable createPage() throws RedirectException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    protected void doRegister() {
+        throw new UnsupportedOperationException();
+
+    }
+
+    @Override
+    public UrlComponent clone() {
+        return new UrlStringBinder(url);
+    }
+
+    @Override
+    protected void constructUrl(StringBuilder sb) {
+        sb.append(url);
+    }
+
+
+}
