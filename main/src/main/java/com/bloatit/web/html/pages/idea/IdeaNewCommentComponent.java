72ec9dac2a80ab77f244663734bb1d701de9ee53
Yoann Plénet <yplenet@gmail.com>
2011-01-07 14:25:21 +0100

    Renaming NewCommentAction to IdeaCommentAction

--- main/src/main/java/com/bloatit/web/html/pages/idea/IdeaNewCommentComponent.java
+++ main/src/main/java/com/bloatit/web/html/pages/idea/IdeaNewCommentComponent.java
@@ -1,7 +1,7 @@
 package com.bloatit.web.html.pages.idea;
 
 import com.bloatit.framework.Demand;
-import com.bloatit.web.actions.NewCommentAction;
+import com.bloatit.web.actions.IdeaCommentAction;
 import com.bloatit.web.html.HtmlElement;
 import com.bloatit.web.html.components.standard.HtmlDiv;
 import com.bloatit.web.html.components.standard.form.HtmlForm;
@@ -9,7 +9,7 @@ import com.bloatit.web.html.components.standard.form.HtmlSubmit;
 import com.bloatit.web.html.components.standard.form.HtmlTextArea;
 import com.bloatit.web.html.pages.master.HtmlPageComponent;
 import com.bloatit.web.server.Context;
-import com.bloatit.web.utils.url.NewCommentActionUrl;
+import com.bloatit.web.utils.url.IdeaCommentActionUrl;
 
 public class IdeaNewCommentComponent extends HtmlPageComponent {
 	private Demand targetIdea;
@@ -21,13 +21,13 @@ public class IdeaNewCommentComponent extends HtmlPageComponent {
     }
 
     protected HtmlElement produce() {
-    	final NewCommentActionUrl url = new NewCommentActionUrl(targetIdea);
+    	final IdeaCommentActionUrl url = new IdeaCommentActionUrl(targetIdea);
         final HtmlDiv commentBlock = new HtmlDiv("new_comment_block");
         
         HtmlForm form = new HtmlForm(url.urlString());
         commentBlock.add(form);
         
-        HtmlTextArea commentInput = new HtmlTextArea(NewCommentAction.COMMENT_CONTENT_CODE, Context.tr("Comment : "), 10, 80);
+        HtmlTextArea commentInput = new HtmlTextArea(IdeaCommentAction.COMMENT_CONTENT_CODE, Context.tr("Comment : "), 10, 80);
         form.add(commentInput);
         
         form.add(new HtmlSubmit(Context.tr("Submit comment")));
