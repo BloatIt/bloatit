dccd1022fc0f0b1016e3201e3378eb72f4012803
Frédéric Bertolus <fred.bertolus@gmail.com>
2011-01-28 11:34:08 +0100

    finish to refactor the design of thz description block of the demand page

--- main/src/main/java/com/bloatit/web/html/pages/demand/DemandTabPane.java
+++ main/src/main/java/com/bloatit/web/html/pages/demand/DemandTabPane.java
@@ -10,17 +10,27 @@
  */
 package com.bloatit.web.html.pages.demand;
 
+import java.util.Locale;
+
 import com.bloatit.common.UnauthorizedOperationException;
+import com.bloatit.framework.Translation;
 import com.bloatit.framework.demand.Demand;
 import com.bloatit.web.annotations.ParamContainer;
 import com.bloatit.web.annotations.RequestParam;
 import com.bloatit.web.html.HtmlNode;
+import com.bloatit.web.html.HtmlTools;
 import com.bloatit.web.html.components.custom.HtmlTabBlock;
 import com.bloatit.web.html.components.custom.HtmlTabBlock.HtmlTab;
+import com.bloatit.web.html.components.custom.renderer.HtmlRawTextRenderer;
+import com.bloatit.web.html.components.standard.HtmlDiv;
+import com.bloatit.web.html.components.standard.HtmlParagraph;
+import com.bloatit.web.html.components.standard.HtmlSpan;
 import com.bloatit.web.html.pages.master.HtmlPageComponent;
 import com.bloatit.web.server.Context;
 import com.bloatit.web.utils.url.DemandPageUrl;
 import com.bloatit.web.utils.url.DemandTabPaneUrl;
+import com.bloatit.web.utils.url.MemberPageUrl;
+import com.bloatit.web.utils.url.PageNotFoundUrl;
 
 @ParamContainer(value = "demandTabPane", isComponent = true)
 public final class DemandTabPane extends HtmlPageComponent {
@@ -48,7 +58,7 @@ public final class DemandTabPane extends HtmlPageComponent {
         tabPane.addTab(new HtmlTab(Context.tr("Description"), "description_tab") {
             @Override
             public HtmlNode generateBody() {
-                return new IdeaDescriptionComponent(demand);
+                return generateDescriptionTabContent(demand);
             }
         });
 
@@ -86,5 +96,65 @@ public final class DemandTabPane extends HtmlPageComponent {
 
         add(tabPane);
 
+    }
+
+    private HtmlNode generateDescriptionTabContent(Demand demand) {
+
+
+        final HtmlDiv descriptionBlock = new HtmlDiv("description_block");
+        {
+            final HtmlDiv descriptionText= new HtmlDiv("description_text");
+            {
+
+                final Locale defaultLocale = Context.getLocalizator().getLocale();
+                try {
+                    final Translation translatedDescription = demand.getDescription().getTranslationOrDefault(defaultLocale);
+                    HtmlParagraph description = new HtmlParagraph(new HtmlRawTextRenderer(translatedDescription.getText()));
+                    descriptionText.add(description);
+                } catch (final UnauthorizedOperationException e1) {
+                    // Nothing.
+                }
+
+
+
+            }
+            descriptionBlock.add(descriptionText);
+
+
+            final HtmlDiv descriptionSeparator= new HtmlDiv("description_separator");
+            descriptionBlock.add(descriptionSeparator);
+
+            final HtmlDiv descriptionFooter= new HtmlDiv("description_footer");
+            {
+                final HtmlDiv descriptionDetails= new HtmlDiv("description_details");
+                {
+                    descriptionDetails.addText(Context.tr("Created by "));
+
+                    try {
+                        final MemberPageUrl memberUrl = new MemberPageUrl(demand.getAuthor());
+                        descriptionDetails.add(memberUrl.getHtmlLink(demand.getAuthor().getDisplayName()));
+                    } catch (final UnauthorizedOperationException e1) {
+                        // Nothing.
+                    }
+
+                    descriptionDetails.addText(Context.tr(" – "));
+
+                    final HtmlSpan dateSpan = new HtmlSpan("description_date");
+                    dateSpan.addText(HtmlTools.formatDate(Context.getLocalizator().getDate(demand.getCreationDate())));
+                    descriptionDetails.add(dateSpan);
+                }
+                descriptionFooter.add(descriptionDetails);
+
+                final HtmlDiv descriptionTranslate= new HtmlDiv("description_translate");
+                {
+                    descriptionTranslate.add(new PageNotFoundUrl().getHtmlLink(Context.tr("Translate")));
+                }
+                descriptionFooter.add(descriptionTranslate);
+
+            }
+            descriptionBlock.add(descriptionFooter);
+
+        }
+        return descriptionBlock;
     }
 }
