c1c955ac90397c22b686f966a361275410cdacf8
Thomas Guyard <thomas.guyard@gmail.com>
2011-01-31 18:02:05 +0100

    fix forgotten file in merge

--- main/src/main/java/com/bloatit/framework/scgiserver/MultipartMime.java
+++ main/src/main/java/com/bloatit/framework/scgiserver/MultipartMime.java
@@ -15,85 +15,73 @@ import com.bloatit.framework.exceptions.NonOptionalParameterException;
 
 /**
  * <p>
- * MultipartMime fields are used to describe complex data in textual form. They
- * are used for mails or as a way to send results from form data (when input
- * contains a file).
+ * MultipartMime fields are used to describe complex data in textual form. They are used
+ * for mails or as a way to send results from form data (when input contains a file).
  * </p>
  * <p>
  * An example of a multipart mime :
- * 
+ *
  * <pre>
  * MIME-Version: 1.0
  * Content-Type: multipart/mixed; boundary="frontier"
- * 
+ *
  * This is a message with multiple parts in MIME format.
  * --frontier
  * Content-Type: text/plain
- * 
+ *
  * This is the body of the message.
  * --frontier
  * Content-Type: application/octet-stream
  * Content-Transfer-Encoding: base64
- * 
+ *
  * PGh0bWw+CiAgPGhlYWQ+CiAgPC9oZWFkPgogIDxib2R5PgogICAgPHA+VGhpcyBpcyB0aGUg
  * Ym9keSBvZiB0aGUgbWVzc2FnZS48L3A+CiAgPC9ib2R5Pgo8L2h0bWw+Cg==
  * --frontier--
  * </pre>
- * 
+ *
  * </p>
  * <p>
- * When constructing a MultipartMime from a byte array, parsing will occur and
- * build a tree containing each elements. Note: class can only handle single
- * level right now, and ignores character encoding.
+ * When constructing a MultipartMime from a byte array, parsing will occur and build a
+ * tree containing each elements. Note: class can only handle single level right now, and
+ * ignores character encoding.
  * </p>
  */
 public class MultipartMime {
-<<<<<<< HEAD:main/src/main/java/com/bloatit/web/scgiserver/MultipartMime.java
-    private static final byte HYPHEN = (byte)'-';
+    private static final byte HYPHEN = (byte) '-';
     private static final byte CR = (byte) '\r';
     private static final byte LF = (byte) '\n';
-    
-    /**
-     * The character sequence used to separate 2 MimeElements in the multipart
-     */
-    private byte[] boundary;
+
     /**
      * the List of mimeElements contained in the multipart
      */
-=======
     private final String contentType;
+    /**
+     * The character sequence used to separate 2 MimeElements in the multipart
+     */
     private final byte[] boundary;
 
->>>>>>> 9d1554ed1686016ec6837bb2442a7fb6d5351a1c:main/src/main/java/com/bloatit/framework/scgiserver/MultipartMime.java
     private final List<MimeElement> elements;
-    /**
-     * The contentType. Can be multipart/mixed, multipart/form-data ...
-     */
-    private String contentType;
 
     /**
      * <p>
      * Creates a multipart mime element based on the given bytes and contentType
      * </p>
      * <p>
-     * The <code>postBytes</code> will be parsed using the <code>boundary</code>
-     * contained in <code>contentType</code>
+     * The <code>postBytes</code> will be parsed using the <code>boundary</code> contained
+     * in <code>contentType</code>
      * </p>
      * <p>
      * Example contentType:
-     * 
+     *
      * <pre>
      * multipart/form-data; boundary=---------------------------19995485819364555411863804163
      * </pre>
      * <p>
-     * 
-     * @param postBytes
-     *            the bytes to parse
-     * @param contentType
-     *            The contentType of the mimeElement, including it's boundary
-     * @throws NonOptionalParameterException
-     *             if any parameter (<code>postBytes</code> or
-     *             <code>contentType</code> is null)
+     *
+     * @param postBytes the bytes to parse
+     * @param contentType The contentType of the mimeElement, including it's boundary
+     * @throws NonOptionalParameterException if any parameter (<code>postBytes</code> or
+     *         <code>contentType</code> is null)
      */
     public MultipartMime(final byte[] postBytes, final String contentType) {
         if (postBytes == null || contentType == null) {
@@ -119,11 +107,9 @@ public class MultipartMime {
      * Name is in fact one of the parameters of the mimeElement header (part of
      * ContentDisposition according to RFC)
      * </p>
-     * 
-     * @param name
-     *            the name of the element to find
-     * @return the element with a matching name, or null if no element has such
-     *         a name
+     *
+     * @param name the name of the element to find
+     * @return the element with a matching name, or null if no element has such a name
      */
     public MimeElement getByName(String name) {
         for (MimeElement me : elements) {
@@ -136,34 +122,15 @@ public class MultipartMime {
     }
 
     /**
-     * Returns a string representation of the content type. This string is build
-     * to be displayed in a console for debug and not to be used in any other
-     * situation. Perform your own tree parsing if you need to display in any
-     * other fashion.
-     */
-    public String toString() {
-        String result = "";
-
-        result += "[CONTENT-TYPE]: " + contentType + "\n";
-        result += "[BOUNDARY]: " + new String(boundary) + "\n";
-        for (MimeElement element : elements) {
-            result += element.toString() + "\n";
-        }
-
-        return result;
-    }
-
-    /**
      * <p>
      * Takes a whole multipart mime and parses it entirely
      * </p>
-     * 
+     *
      * @param postBytes the bytes to parse (i.e. the multipart mime)
      * @param contentType The content type String for the mime element, including the
      *        boundary
      * @return a Parsed MimeMultipart
      */
-<<<<<<< HEAD:main/src/main/java/com/bloatit/web/scgiserver/MultipartMime.java
     private void parseMultipart(final byte[] postBytes, String contentType) {
         // An example of multipart Mime :
         //
@@ -175,26 +142,20 @@ public class MultipartMime {
         //
         // This is the body of the message. --frontier
         // Content-Type: application/octet-stream Content-Transfer-Encoding:
-        //  base64
+        // base64
         //
         // PGh0bWw+CiAgPGhlYWQ+CiAgPC9oZWFkPgogIDxib2R5PgogICAgPHA+VGhpcyBpcyB0aGUg
         // Ym9keSBvZiB0aGUgbWVzc2FnZS48L3A+CiAgPC9ib2R5Pgo8L2h0bWw+Cg==
         // --frontier--
         //
         // Algorithm used :
-        // - We try to spot any occurence of double hyphens 
+        // - We try to spot any occurence of double hyphens
         // - Whenever we find double hyphens, we check if next part matches boundary
         // - If it does match boundary, we move to the next part of the element
         // - If it doesn't, it means we are still into the previous element
         ByteArrayInputStream input = new ByteArrayInputStream(postBytes);
         ArrayList<Byte> boundBuff = new ArrayList<Byte>();
         ArrayList<Byte> contentBuff = new ArrayList<Byte>();
-=======
-    private void parseMultipart(final byte[] postBytes, final String contentType) {
-        final ByteArrayInputStream input = new ByteArrayInputStream(postBytes);
-        final ArrayList<Byte> boundBuff = new ArrayList<Byte>();
-        final ArrayList<Byte> contentBuff = new ArrayList<Byte>();
->>>>>>> 9d1554ed1686016ec6837bb2442a7fb6d5351a1c:main/src/main/java/com/bloatit/framework/scgiserver/MultipartMime.java
         int hyphenRead = 0;
         byte b;
         int i = 0;
@@ -246,18 +207,12 @@ public class MultipartMime {
      * <p>
      * Parses a multipartContent
      * </p>
-     * 
-     * @param content
-     *            the content to parse
+     *
+     * @param content the content to parse
      */
-<<<<<<< HEAD:main/src/main/java/com/bloatit/web/scgiserver/MultipartMime.java
-    private void parseMultipartContent(List<Byte> content) {
-
-=======
     private void parseMultipartContent(final List<Byte> content) {
         final byte CR = (byte) '\r';
         final byte LF = (byte) '\n';
->>>>>>> 9d1554ed1686016ec6837bb2442a7fb6d5351a1c:main/src/main/java/com/bloatit/framework/scgiserver/MultipartMime.java
 
         if (content.size() > 0) {
             boolean foundCR = false;
@@ -314,22 +269,16 @@ public class MultipartMime {
         }
     }
 
-<<<<<<< HEAD:main/src/main/java/com/bloatit/web/scgiserver/MultipartMime.java
     /**
      * <p>
      * Parses the header of a MultipartMime content
      * </p>
-     * 
-     * @param headerBytes
-     *            the bytes containing the header
+     *
+     * @param headerBytes the bytes containing the header
      * @return a map of header-name -> value
      */
     private Map<String, String> parseHeader(List<Byte> headerBytes) {
-        Map<String, String> result = new HashMap<String, String>();
-=======
-    private Map<String, String> parseHeader(final List<Byte> headerBytes) {
         final Map<String, String> result = new HashMap<String, String>();
->>>>>>> 9d1554ed1686016ec6837bb2442a7fb6d5351a1c:main/src/main/java/com/bloatit/framework/scgiserver/MultipartMime.java
 
         final byte[] bytes = new byte[headerBytes.size()];
         int i = 0;
@@ -363,9 +312,12 @@ public class MultipartMime {
 
         return result;
     }
-<<<<<<< HEAD:main/src/main/java/com/bloatit/web/scgiserver/MultipartMime.java
-=======
 
+    /**
+     * Returns a string representation of the content type. This string is build to be
+     * displayed in a console for debug and not to be used in any other situation. Perform
+     * your own tree parsing if you need to display in any other fashion.
+     */
     @Override
     public String toString() {
         String result = "";
@@ -378,5 +330,4 @@ public class MultipartMime {
 
         return result;
     }
->>>>>>> 9d1554ed1686016ec6837bb2442a7fb6d5351a1c:main/src/main/java/com/bloatit/framework/scgiserver/MultipartMime.java
 }
