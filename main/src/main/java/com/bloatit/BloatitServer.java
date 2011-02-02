641352b1e3ad8ccfbcb547e61659592ea3f5f73b
Thomas Guyard <thomas.guyard@gmail.com>
2011-02-02 15:52:01 +0100

    fix bug in the main

--- main/src/main/java/com/bloatit/BloatitServer.java
+++ main/src/main/java/com/bloatit/BloatitServer.java
@@ -9,10 +9,13 @@ public class BloatitServer {
     public static void main(String[] args) {
         Framework framework = new Framework(new Model());
         framework.addProcessor(new BloatitWebServer());
+        
+        framework.initialize();
         try {
             framework.run();
         } finally {
             framework.shutdown();
+            System.exit(0);
         }
     }
 
