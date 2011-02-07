22445dc404fd6c1f0a65983a4835487de78960da
Yoann <yplenet@gmail.com>
2011-02-07 14:26:29 +0100

    Adding Multi part test class (empty atm)

--- main/src/main/java/com/bloatit/framework/webserver/mime/MultipartMimeParser.java
+++ main/src/main/java/com/bloatit/framework/webserver/mime/MultipartMimeParser.java
@@ -354,44 +354,5 @@ public class MultipartMimeParser {
     public boolean available() {
         return currentState != State.END;
     }
-//    public static void main(String[] args) throws MimeTypeParseException, IOException, InvalidMimeEncodingException, MalformedMimeException {
-//        // String toParse =
-//        // "-----------------------------112924560112292093081138430544\r\n" +
-//        // //
-//        // "Content-Disposition: form-data; name=\"wow\"\r\n" + //
-//        // "\r\n" + //
-//        // "dqsd qsd qsd \r\n" + //
-//        // "-----------------------------112924560112292093081138430544\r\n" +
-//        // //
-//        // "Content-Disposition: form-data; name=\"fichier\"; filename=\"test.upload\"\r\n"
-//        // + //
-//        // "Content-Type: application/octet-stream\r\n" + //
-//        // "\r\n" + //
-//        // "qsd qd qsd qsd qsd qsd sd qsd qsd z sdsqqqqqqqqqqqq sqd        qsddddddddddddddddd qsd qsssssss\n"
-//        // + //
-//        // "qsd\nqsd qsabcd \n" + //
-//        // "sdq aerf df sdf sa\n\r\n" + //
-//        // "-----------------------------112924560112292093081138430544--"; //
-//
-//        String toParse = "This is a message with multiple parts in MIME format.\r\n" + "--frontier\r\n" + "Content-Type: text/plain\r\n" + "\r\n"
-//                + "This is the body of the message.\r\n" + "--frontier\r\n" + "Content-Type: application/octet-stream; filename=\"boo\"\r\n"
-//                + "Content-Transfer-Encoding: base64\r\n" + "\r\n" + "PGh0bWw+CiAgPGhlYWQ+CiAgPC9oZWFkPgogIDxib2R5PgogICAgPHA+VGhpcyBpcyB0aGUg"
-//                + "Ym9keSBvZiB0aGUgbWVzc2FnZS48L3A+CiAgPC9ib2R5Pgo8L2h0bWw+Cg==\r\n" + "--frontier--";
-//
-//        String contentType = "Content-Type: multipart/mixed; boundary=frontier";
-//        // String contentType =
-//        // "multipart/form-data; boundary=---------------------------112924560112292093081138430544";
-//        System.out.println(contentType);
-//        System.out.println(toParse);
-//        ByteArrayInputStream bais = new ByteArrayInputStream(toParse.getBytes());
-//
-//        MultipartMimeParser mmp = new MultipartMimeParser(bais, contentType, new UUIDFileNameGenerator(), UPLOAD_TEMP_DIRECTORY);
-//        MimeElement me;
-//        while ((me = mmp.readContent()) != null) {
-//            System.out.println("CONTENT");
-//            System.out.println(me);
-//            System.out.println("[ENCODING !! : ]" + me.getEncoding());
-//        }
-//        System.out.println("DONE");
-//    }
+
 }
