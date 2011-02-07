365be9cfce5fb1537389c9a337ecc532f5a0a830
Frédéric Bertolus <fred.bertolus@gmail.com>
2011-02-07 23:31:24 +0100

    add new scgi engine

---
+++ main/src/main/java/com/bloatit/framework/fcgi/FCGIPostStream.java
@@ -0,0 +1,63 @@
+package com.bloatit.framework.fcgi;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+
+public class FCGIPostStream extends InputStream {
+
+    PipedInputStream pipeIn;
+    PipedOutputStream pipeOut;
+    private final FCGIParser fcgiParser;
+
+    public FCGIPostStream(FCGIParser fcgiParser) throws IOException {
+        this.fcgiParser = fcgiParser;
+        pipeIn = new PipedInputStream(66000);
+        pipeOut = new PipedOutputStream(pipeIn);
+    }
+
+    @Override
+    public int read() throws IOException {
+        byte[] b = new byte[1];
+        if (read(b) == 1) {
+            int plop =  (0x000000FF & (b[0]));
+            if(plop == -1) {
+                System.err.println("blabla");
+            }
+            return plop;
+        }
+        return -1;
+    }
+
+    @Override
+    public int read(byte[] b, int off, int len) throws IOException {
+        if (len == 0) {
+            return 0;
+        }
+        if (pipeIn.available() == 0) {
+            if (fcgiParser.isPostStreamOpen()) {
+                fcgiParser.fetchPostRecord();
+            } else {
+                return -1;
+            }
+            if (!fcgiParser.isPostStreamOpen() || pipeIn.available() == 0) {
+                return -1;
+            }
+        }
+        int plop = pipeIn.read(b, off, len);
+        if(plop == -1){
+            System.out.println("PLOP");
+        }
+        return plop;
+    }
+
+    @Override
+    public int read(byte[] b) throws IOException {
+        return read(b, 0, b.length);
+    }
+
+    public void pushData(byte[] data) throws IOException {
+        pipeOut.write(data);
+    }
+}
