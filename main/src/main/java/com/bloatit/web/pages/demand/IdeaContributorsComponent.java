c258a2774eba7fee7ff5386f900400df98426be4
Frédéric Bertolus <fred.bertolus@gmail.com>
2011-02-14 15:08:24 +0100

    improve contribution table

--- main/src/main/java/com/bloatit/web/pages/demand/IdeaContributorsComponent.java
+++ main/src/main/java/com/bloatit/web/pages/demand/IdeaContributorsComponent.java
@@ -10,35 +10,30 @@
  */
 package com.bloatit.web.pages.demand;
 
-import static com.bloatit.framework.webserver.Context.tr;
-
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
+import java.util.List;
 
 import com.bloatit.framework.exceptions.UnauthorizedOperationException;
 import com.bloatit.framework.utils.PageIterable;
+import com.bloatit.framework.utils.i18n.DateLocale.FormatStyle;
 import com.bloatit.framework.webserver.Context;
 import com.bloatit.framework.webserver.annotations.ParamContainer;
 import com.bloatit.framework.webserver.components.HtmlDiv;
-import com.bloatit.framework.webserver.components.HtmlParagraph;
+import com.bloatit.framework.webserver.components.HtmlTitle;
 import com.bloatit.framework.webserver.components.advanced.HtmlTable;
 import com.bloatit.framework.webserver.components.advanced.HtmlTable.HtmlTableModel;
 import com.bloatit.framework.webserver.components.meta.HtmlElement;
 import com.bloatit.model.Contribution;
 import com.bloatit.model.Demand;
-import com.bloatit.web.components.HtmlPagedList;
 import com.bloatit.web.url.IdeaContributorsComponentUrl;
 
 @ParamContainer(value = "DemandContributorsComponent", isComponent = true)
 public final class IdeaContributorsComponent extends HtmlDiv {
 
-    private int contributionCount;
-    private HtmlParagraph contributionMin;
-    private HtmlParagraph contributionMax;
-    private HtmlParagraph contributionMean;
-    private PageIterable<Contribution> contributions;
     private final Demand demand;
-    private HtmlPagedList<Contribution> participationsList;
 
     public IdeaContributorsComponent(final IdeaContributorsComponentUrl url, final Demand demand) {
         super();
@@ -53,53 +48,80 @@ public final class IdeaContributorsComponent extends HtmlDiv {
     }
 
     protected HtmlElement produce(IdeaContributorsComponentUrl url) {
-        final HtmlDiv contributorsBlock = new HtmlDiv("contributors_block");
+        final HtmlDiv contributorsBlock = new HtmlDiv("contribution_block");
         {
 
-            // Display contribution count
-            contributorsBlock.add(new HtmlParagraph(contributionCount + tr(" contributions")));
+            try {
+                int contributionCount;
 
-            // Display contribution stats
-            if (contributionCount > 0) {
-                contributorsBlock.add(contributionMin);
-                contributorsBlock.add(contributionMax);
-                contributorsBlock.add(contributionMean);
+                contributionCount = demand.getContributions().size();
+
+                // Display contribution count
+                contributorsBlock.add(new HtmlTitle(Context.trn("{0} contribution", "{0} contributions", contributionCount, contributionCount), 1));
+
+                // TODO: generate contribution graph
+
+                // Display contribution stats
+                if (contributionCount > 0) {
+                    final String contributionMeanValue = Context.getLocalizator()
+                            .getCurrency(demand.getContribution().divide(new BigDecimal(contributionCount))).getDefaultString();
+                    final String contributionMinValue = Context.getLocalizator().getCurrency(demand.getContributionMin()).getDefaultString();
+                    final String contributionMaxValue = Context.getLocalizator().getCurrency(demand.getContributionMax()).getDefaultString();
+                    final String contributionMedianValue = Context.getLocalizator().getCurrency(computeMedian(demand.getContributions()))
+                            .getDefaultString();
+
+                    HtmlTable statTable = new HtmlTable(new ContributionStatTableModel(contributionMinValue, contributionMaxValue,
+                            contributionMeanValue, contributionMedianValue));
+                    contributorsBlock.add(statTable);
+                }
+
+                HtmlTable table = new HtmlTable(new ContributionTableModel(demand.getContributions()));
+                contributorsBlock.add(table);
+
+            } catch (UnauthorizedOperationException e) {
             }
-
-            HtmlTable table = new HtmlTable(new  ContributionTableModel(contributions));
-
-            contributorsBlock.add(table);
 
         }
         return contributorsBlock;
     }
 
-    protected void extractData() throws UnauthorizedOperationException {
+    private BigDecimal computeMedian(PageIterable<Contribution> contributions) {
 
-        contributionCount = demand.getContributions().size();
+        try {
 
-        if (contributionCount > 0) {
+            Iterator<Contribution> it = contributions.iterator();
 
-            final String contributionMeanValue = Context.getLocalizator().getCurrency(demand.getContribution().divide(new BigDecimal(contributionCount))).getDefaultString();
-            final String contributionMinValue = Context.getLocalizator().getCurrency(demand.getContributionMin()).getDefaultString();
-            final String contributionMaxValue = Context.getLocalizator().getCurrency(demand.getContributionMax()).getDefaultString();
+            List<BigDecimal> list = new ArrayList<BigDecimal>();
 
-            contributionMin = new HtmlParagraph(tr("Min: ") + contributionMinValue);
-            contributionMax = new HtmlParagraph(tr("Max: ") + contributionMaxValue);
-            contributionMean = new HtmlParagraph(tr("Mean: ") + contributionMeanValue);
+            while (it.hasNext()) {
+                list.add(it.next().getAmount());
+            }
+            Collections.sort(list);
+
+            int middle = list.size() / 2;
+
+            if (list.size() % 2 == 1) {
+                return list.get(middle);
+            } else {
+                return list.get(middle-1).add(list.get(middle)).divide(new BigDecimal(2));
+            }
+
+        } catch (UnauthorizedOperationException e) {
+            return new BigDecimal(-1);
         }
-
-        contributions = demand.getContributions();
 
     }
 
+    protected void extractData() throws UnauthorizedOperationException {
 
-    private static final class ContributionTableModel implements HtmlTableModel {
+    }
+
+    public static class ContributionTableModel extends HtmlTableModel {
         private Iterator<Contribution> it;
         private Contribution contribution;
         private final PageIterable<Contribution> contributions;
 
-        private ContributionTableModel(PageIterable<Contribution> contributions) {
+        public ContributionTableModel(PageIterable<Contribution> contributions) {
             this.contributions = contributions;
 
         }
@@ -134,7 +156,7 @@ public final class IdeaContributorsComponent extends HtmlDiv {
 
         @Override
         public boolean next() {
-            if(it == null) {
+            if (it == null) {
                 it = contributions.iterator();
             }
 
@@ -143,6 +165,15 @@ public final class IdeaContributorsComponent extends HtmlDiv {
                 return true;
             }
             return false;
+        }
+
+
+        @Override
+        public String getColumnCss(int column) {
+            if(column == 1) {
+                return "money_cell";
+            }
+            return null;
         }
 
         @Override
@@ -154,11 +185,11 @@ public final class IdeaContributorsComponent extends HtmlDiv {
                     value = contribution.getAuthor().getDisplayName();
                     break;
                 case 1:
-                    //TODO: align money at right in CSS
+                    // TODO: align money at right in CSS
                     value = Context.getLocalizator().getCurrency(contribution.getAmount()).getDefaultString();
                     break;
                 case 2:
-                    value = contribution.getCreationDate().toString();
+                    value = Context.getLocalizator().getDate(contribution.getCreationDate()).toString(FormatStyle.MEDIUM);
                     break;
                 case 3:
                     value = contribution.getComment();
@@ -166,11 +197,100 @@ public final class IdeaContributorsComponent extends HtmlDiv {
                 }
 
             } catch (UnauthorizedOperationException e) {
-                //Display nothing
+                // Display nothing
             }
 
-            if(value == null) {
+            if (value == null) {
                 value = "";
+            }
+            return value;
+        }
+    }
+
+    public static final class ContributionStatTableModel extends HtmlTableModel {
+
+        private final String contributionMinValue;
+        private final String contributionMaxValue;
+        private final String contributionMeanValue;
+        private int line = -1;
+        private final int lineCount = 1;
+        private final String contributionMedianValue;
+
+        private ContributionStatTableModel(String contributionMinValue, String contributionMaxValue, String contributionMeanValue,
+                String contributionMedianValue) {
+
+            this.contributionMinValue = contributionMinValue;
+            this.contributionMaxValue = contributionMaxValue;
+            this.contributionMeanValue = contributionMeanValue;
+            this.contributionMedianValue = contributionMedianValue;
+
+        }
+
+        @Override
+        public boolean hasHeader() {
+            return true;
+        }
+
+        @Override
+        public int getColumnCount() {
+            return 4;
+        }
+
+
+
+        @Override
+        public String getColumnCss(int column) {
+            return "money_cell";
+        }
+
+        @Override
+        public String getHeader(int column) {
+            String value = "";
+
+            switch (column) {
+            case 0:
+                value = Context.tr("Minimun");
+                break;
+            case 1:
+                value = Context.tr("Maximum");
+                break;
+            case 2:
+                value = Context.tr("Mean");
+                break;
+            case 3:
+                value = Context.tr("Median");
+                break;
+            }
+            return value;
+        }
+
+        @Override
+        public boolean next() {
+            line++;
+            if (line < lineCount) {
+                return true;
+            }
+
+            return false;
+        }
+
+        @Override
+        public String getBody(int column) {
+            String value = "";
+
+            switch (column) {
+            case 0:
+                value = contributionMinValue;
+                break;
+            case 1:
+                value = contributionMaxValue;
+                break;
+            case 2:
+                value = contributionMeanValue;
+                break;
+            case 3:
+                value = contributionMedianValue;
+                break;
             }
             return value;
         }
