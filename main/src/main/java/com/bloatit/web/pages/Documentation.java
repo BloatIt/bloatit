19c71ad1a28cf79b8f44790568f9610474460ce1
Frédéric Bertolus <fred.bertolus@gmail.com>
2011-02-24 17:21:32 +0100

    update documentation renderer system

--- main/src/main/java/com/bloatit/web/pages/Documentation.java
+++ main/src/main/java/com/bloatit/web/pages/Documentation.java
@@ -1,25 +1,16 @@
 package com.bloatit.web.pages;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Collections;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-
-import com.bloatit.common.Log;
-import com.bloatit.framework.exceptions.FatalErrorException;
 import com.bloatit.framework.exceptions.RedirectException;
 import com.bloatit.framework.webserver.Context;
+import com.bloatit.framework.webserver.PageNotFoundException;
 import com.bloatit.framework.webserver.annotations.Optional;
 import com.bloatit.framework.webserver.annotations.ParamContainer;
 import com.bloatit.framework.webserver.annotations.RequestParam;
-import com.bloatit.framework.webserver.components.HtmlDiv;
-import com.bloatit.framework.webserver.components.renderer.HtmlMarkdownRenderer;
-import com.bloatit.web.WebConfiguration;
+import com.bloatit.web.pages.documentation.HtmlDocumentationRenderer;
+import com.bloatit.web.pages.documentation.HtmlDocumentationRenderer.DocumentationType;
+import com.bloatit.web.pages.master.BoxLayout;
 import com.bloatit.web.pages.master.MasterPage;
+import com.bloatit.web.pages.master.TwoColumnLayout;
 import com.bloatit.web.url.DocumentationUrl;
 
 /**
@@ -38,13 +29,6 @@ public class Documentation extends MasterPage {
     private final static String DOC_TARGET = "doc";
     private final static String DEFAULT_DOC = "home";
 
-    /**
-     * <p>
-     * Store the html documents (after they've been converted from markdown)
-     * </p>
-     */
-    private static Map<MarkdownDocumentationMarker, MarkdownDocumentationContent> cache = Collections.synchronizedMap((new HashMap<MarkdownDocumentationMarker, MarkdownDocumentationContent>()));
-
     @RequestParam(name = DOC_TARGET)
     @Optional(DEFAULT_DOC)
     private final String docTarget;
@@ -61,52 +45,21 @@ public class Documentation extends MasterPage {
 
     @Override
     protected void doCreate() throws RedirectException {
-        final String dir = WebConfiguration.getBloatitDocumentationDir();
-        final HtmlDiv master = new HtmlDiv("padding_box");
-        add(master);
 
-        FileInputStream fis;
-        final String language = Context.getLocalizator().getLanguageCode();
-        try {
-            File targetFile = new File(dir + "/" + docTarget + "_" + language);
+         TwoColumnLayout layout = new TwoColumnLayout();
 
-            if (!targetFile.exists()) {
-                Log.web().warn("User tried to access doc file " + docTarget + "_" + language + " but it doesn't exist.");
-                session.notifyBad(Context.tr("Documentation file {0} doesn''t exist in language {1}, using english instead", docTarget, language));
-                targetFile = new File(dir + docTarget + "_" + "en");
-            }
-            fis = new FileInputStream(targetFile);
 
-            final MarkdownDocumentationMarker mdm = new MarkdownDocumentationMarker(docTarget, language);
-            final MarkdownDocumentationContent mdc = cache.get(mdm);
+         BoxLayout box = new BoxLayout();
 
-            if (mdc == null || mdc.savedDate.before(new Date(targetFile.lastModified()))) {
-                // No content, or content has been saved before the file was
-                // last modified
-                Log.web().trace("Reading from the markdown documentation file " + docTarget);
-                final byte[] b = new byte[fis.available()];
-                fis.read(b);
-                final String markDownContent = new String(b);
-                final HtmlMarkdownRenderer content = new HtmlMarkdownRenderer(markDownContent);
-                cache.put(mdm, new MarkdownDocumentationContent(new Date(), content.getRendereredContent()));
-                master.add(content);
-            } else {
-                Log.web().trace("Using cache for documentation file " + docTarget);
-                master.add(new HtmlMarkdownRenderer(mdc.htmlString, true));
-            }
+         HtmlDocumentationRenderer docRenderer = new HtmlDocumentationRenderer(DocumentationType.MAIN_DOC, docTarget);
+         if(! docRenderer.isExists()) {
+             throw new PageNotFoundException();
+         }
 
-        } catch (final FileNotFoundException e) {
-            // User asked a wrong documentation file, redirecting him to the doc
-            // home
-            Log.web().warn("A user tries to access documentation file " + docTarget + " but file is not available.");
-            session.notifyBad(Context.tr("Documentation entry {0} doesn''t exist. Sending you to documentation home page", docTarget));
+         box.add(docRenderer);
+         layout.addLeft(box);
 
-            final DocumentationUrl redirectTo = new DocumentationUrl();
-            redirectTo.setDocTarget(DEFAULT_DOC);
-            throw new RedirectException(redirectTo);
-        } catch (final IOException e) {
-            throw new FatalErrorException("An error occured while parsing the documentation file " + docTarget, e);
-        }
+         add(layout);
     }
 
     @Override
@@ -114,69 +67,5 @@ public class Documentation extends MasterPage {
         return Context.tr("Elveos documentation: {0}", docTarget);
     }
 
-    /**
-     * Nested class used as a key to cache parsed content
-     */
-    private class MarkdownDocumentationMarker {
-        public String name;
-        public String lang;
 
-        @Override
-        public int hashCode() {
-            final int prime = 31;
-            int result = 1;
-            result = prime * result + ((lang == null) ? 0 : lang.hashCode());
-            result = prime * result + ((name == null) ? 0 : name.hashCode());
-            return result;
-        }
-
-        @Override
-        public boolean equals(final Object obj) {
-            if (this == obj) {
-                return true;
-            }
-            if (obj == null) {
-                return false;
-            }
-            if (getClass() != obj.getClass()) {
-                return false;
-            }
-            final MarkdownDocumentationMarker other = (MarkdownDocumentationMarker) obj;
-            if (lang == null) {
-                if (other.lang != null) {
-                    return false;
-                }
-            } else if (!lang.equals(other.lang)) {
-                return false;
-            }
-            if (name == null) {
-                if (other.name != null) {
-                    return false;
-                }
-            } else if (!name.equals(other.name)) {
-                return false;
-            }
-            return true;
-        }
-
-        public MarkdownDocumentationMarker(final String name, final String lang) {
-            super();
-            this.name = name;
-            this.lang = lang;
-        }
-    }
-
-    /**
-     * Nested class used as a MapEntry.value to cache parsed markdown content
-     */
-    private class MarkdownDocumentationContent {
-        public Date savedDate;
-        public String htmlString;
-
-        public MarkdownDocumentationContent(final Date savedDate, final String htmlString) {
-            super();
-            this.savedDate = savedDate;
-            this.htmlString = htmlString;
-        }
-    }
 }
