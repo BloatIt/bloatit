4121c049a29605dcdfe46a1c5ed70ed53254a82d
Yoann <yplenet@gmail.com>
2011-02-25 12:04:09 +0100

    Adding missing managers and lists

---
+++ main/src/main/java/com/bloatit/model/lists/InternalAccountList.java
@@ -0,0 +1,30 @@
+//
+// Copyright (c) 2011 Linkeos.
+//
+// This file is part of Elveos.org.
+// Elveos.org is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+//
+// Elveos.org is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+// more details.
+// You should have received a copy of the GNU General Public License along
+// with Elveos.org. If not, see http://www.gnu.org/licenses/.
+//
+package com.bloatit.model.lists;
+
+import com.bloatit.data.DaoInternalAccount;
+import com.bloatit.framework.utils.PageIterable;
+import com.bloatit.model.InternalAccount;
+
+/**
+ * The Class InternalAccountList transforms PageIterable<DaoInternalAccount> to PageIterable<InternalAccount>.
+ */
+public final class InternalAccountList extends ListBinder<InternalAccount, DaoInternalAccount> {
+    public InternalAccountList(PageIterable<DaoInternalAccount> daoCollection) {
+        super(daoCollection);
+    }
+}
