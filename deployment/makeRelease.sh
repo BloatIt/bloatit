c08454abb8723fb2104d98405802a8b36340de3d
Thomas Guyard <thomas.guyard@gmail.com>
2011-03-24 19:33:14 +0100

    Add the possibility to release using a commit

--- deployment/makeRelease.sh
+++ deployment/makeRelease.sh
@@ -3,7 +3,7 @@
 usage()
 {
 cat << EOF
-usage: "$0" -d host -r releaseVersion -s nextSnapshotVersion -b bloatitFolder [-n name] [-h] 
+usage: "$0" -d host -r releaseVersion { -s nextSnapshotVersion | -t } -b bloatitFolder [-n name] [-h] 
 
 This script create a release, tag it and send it to a distant host.
 
@@ -14,7 +14,7 @@ OPTIONS:
    -d      Destination host. Requiered.
    -b      Bloatit root folder (git/mvn root).
    -n      Distant user name. Default is "bloatit".
-   -t      Skip tests in maven.
+   -t      Use tagged version.
 EOF
 }
 
@@ -26,7 +26,7 @@ HOST=
 RELEASE_VERSION=
 NEXT_SNAPSHOT_VERSION=
 REPOS_DIR=
-SKIP_TEST=
+USE_TAG=
 USER=bloatit
 
 while getopts "thd:b:n:r:s:" OPTION
@@ -49,7 +49,7 @@ do
              REPOS_DIR=$OPTARG
              ;;
          t)
-             SKIP_TEST="-Dmaven.test.skip"
+             USE_TAG="true"
              ;;
          n)
              USER=$OPTARG
@@ -61,13 +61,12 @@ do
      esac
 done
 
-if [ -z "$HOST" ] || [ -z "$RELEASE_VERSION" ] || [ -z "$NEXT_SNAPSHOT_VERSION" ] || [ -z "$REPOS_DIR" ]
+if [ -z "$HOST" ] || [ -z "$RELEASE_VERSION" ] || [ -z "$REPOS_DIR" ]
 then
 	echo -e "Arguments are missing !!! \n" 1>&2
 	usage 1>&2
 	exit 1
 fi
-
 
 . $PWD/log.sh
 . $PWD/conf.sh
@@ -75,24 +74,35 @@ fi
 
 calculateLogFilename # We can know use the LOG_FILE variable.
 PREFIX=bloatit
-MVN="mvn -f $REPOS_DIR/pom.xml $SKIP_TEST"
+MVN="mvn -f $REPOS_DIR/pom.xml" 
 SSH="ssh -t $USER@$HOST"
 LIQUIBASE_DIR=$REPOS_DIR/main/liquibase/liquibase-core-2.0.2-SNAPSHOT.jar
-
 
 echo "HOST=$HOST
 RELEASE_VERSION=$RELEASE_VERSION
 NEXT_SNAPSHOT_VERSION=$NEXT_SNAPSHOT_VERSION
 REPOS_DIR=$REPOS_DIR
-SKIP_TEST=$SKIP_TEST
+USE_TAG=$USE_TAG
 USER=$USER"
-
 log_ok "You are about to create a new release and send it to a distant server" $LOG_FILE
 abort_if_non_zero $?
 
-performMvnRelease "$LOG_FILE" "$PREFIX" "$RELEASE_VERSION" "$NEXT_SNAPSHOT_VERSION" "$MVN"
+if [ -z "$USE_TAG" ] ; then
+    performMvnRelease "$LOG_FILE" "$PREFIX" "$RELEASE_VERSION" "$NEXT_SNAPSHOT_VERSION" "$MVN"
+    transferData "$LOG_FILE" "$HOST" "$REPOS_DIR" "$USER"
+else
+    log_date "Using the tag: $PREFIX-$RELEASE_VERSION" "$LOG_FILE"
+    if [ -n "$( git status --porcelain)" ] ; then
+	echo "You have non commited data !"
+        exit_fail
+    fi
+    git checkout "$PREFIX-$RELEASE_VERSION"
+    transferData "$LOG_FILE" "$HOST" "$REPOS_DIR" "$USER"
+    git checkout HEAD
+    git checkout master
+    echo "WARNING: Going back to master !! "
+fi
 
-transferData "$LOG_FILE" "$HOST" "$REPOS_DIR" "$USER"
 
 commitPrerelease "$LOG_FILE" "$PREFIX" "$RELEASE_VERSION" "$SSH"
 
