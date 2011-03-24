398abd8dcd2ef515e67232effabdcd1a0cc63a80
Thomas Guyard <thomas.guyard@gmail.com>
2011-03-24 19:01:05 +0100

    Update the deployment scripts

--- deployment/makeRelease.sh
+++ deployment/makeRelease.sh
@@ -3,7 +3,7 @@
 usage()
 {
 cat << EOF
-usage: $0 -d host -r releaseVersion -s nextSnapshotVersion -b bloatitFolder [-n name] [-h] 
+usage: "$0" -d host -r releaseVersion -s nextSnapshotVersion -b bloatitFolder [-n name] [-h] 
 
 This script create a release, tag it and send it to a distant host.
 
@@ -68,38 +68,17 @@ then
 	exit 1
 fi
 
-PREFIX=bloatit
-MVN="mvn -f $REPOS_DIR/pom.xml $SKIP_TEST"
 
 . $PWD/log.sh
 . $PWD/conf.sh
+. $PWD/releaseUtils.sh
 
-SSH="ssh $USER@$HOST"
+calculateLogFilename # We can know use the LOG_FILE variable.
+PREFIX=bloatit
+MVN="mvn -f $REPOS_DIR/pom.xml $SKIP_TEST"
+SSH="ssh -t $USER@$HOST"
+LIQUIBASE_DIR=$REPOS_DIR/main/liquibase/liquibase-core-2.0.2-SNAPSHOT.jar
 
-exit_ok(){
-    echo "exiting"
-    exit 0
-}
-
-exit_fail(){
-    echo "Failure. Abording know ! "
-    kill $$
-    exit 128
-}
-
-abort(){
-    echo "aborting"
-    exit 255
-}
-
-abort_if_non_zero(){
-    if [ $1 != "0" ] ; then 
-        abort
-    fi
-}
-
-calculateLogFilename
-# We can know use the LOG_FILE variable.
 
 echo "HOST=$HOST
 RELEASE_VERSION=$RELEASE_VERSION
@@ -111,101 +90,20 @@ USER=$USER"
 log_ok "You are about to create a new release and send it to a distant server" $LOG_FILE
 abort_if_non_zero $?
 
-##
-## Perform the mvn release.
-##
+performMvnRelease "$LOG_FILE" "$PREFIX" "$RELEASE_VERSION" "$NEXT_SNAPSHOT_VERSION" "$MVN"
 
-log_date "Make a mvn release." $LOG_FILE
-(
-    $MVN release:clean
-    $MVN install -Dmaven.test.skip=true
-    $MVN --batch-mode -Dtag=$PREFIX-$RELEASE_VERSION release:prepare \
-                      -DreleaseVersion=$RELEASE_VERSION \
-		      -DdevelopmentVersion=$NEXT_SNAPSHOT_VERSION-SNAPSHOT \
-		      -DautoVersionSubmodules=true \
-    && $MVN release:clean
-# Do not do the perform.
-# Just clean if there is no errors
+transferData "$LOG_FILE" "$HOST" "$REPOS_DIR" "$USER"
 
-   [ "$?" = "0" ] || exit_fail
+commitPrerelease "$LOG_FILE" "$PREFIX" "$RELEASE_VERSION" "$SSH"
 
-) | tee -a $LOG_FILE
+stopBloatitServer "$LOG_FILE" "$SSH"
 
+migratingDB "$LOG_FILE" "$LIQUIBASE_DIR" "$USER" "$SSH"
 
+propagateConfFiles "$LOG_FILE" "$UP_CONF_DIR" "$CONF_DIR" "$UP_SHARE_DIR" "$SHARE_DIR" "$UP_RESSOURCES" "$CLASSES" "$SSH"
 
+commitRelease "$LOG_FILE" "$PREFIX" "$RELEASE_VERSION" "$SSH"
 
-##
-## Send newly added data to the distant server
-##
-
-./transfert.sh -d $HOST -l $LOG_FILE -s $REPOS_DIR -n $USER
-
-[ $? = 0 ] || exit_fail
-
-##
-## Commit the git distant new data
-##
-
-$SSH "
-git status
-git add -A
-git commit -m \"New PreRelease $PREFIX-$RELEASE_VERSION\"
-"
-
-##
-## Stopping the server.
-##
-log_date "Stopping the bloatit server." $LOG_FILE
-(
-    $SSH "/etc/init.d/bloatit stop && sleep 2"
-
-    [ $? = 0 ] || exit_fail
-
-) | tee -a $LOG_FILE
-
-
-##
-## Migrating DB.
-##
-(
-LIQUIBASE_DIR=$REPOS_DIR/main/liquibase/liquibase-core-2.0.2-SNAPSHOT.jar
-cat $LIQUIBASE_DIR | $SSH "
-cat > /tmp/$(basename $LIQUIBASE_DIR)
-cd /home/$USER/java/
-java -jar /tmp/$(basename $LIQUIBASE_DIR) \
-    --classpath=.:/home/$USER/jar/dom4j*.jar:/home/$USER/jar/postgresql*.jar:/home/$USER/jar/sl4j-api*.jar:/home/$USER/jar/slfj-jdk*.jar \
-"
-    [ $? = 0 ] || exit_fail
-) | tee -a $LOG_FILE
-
-##
-## Propagate conf files.
-##
-#remote execute:
-(
-MERGE_FILE_SCRIPT=mergeFiles.sh
-cat ./$MERGE_FILE_SCRIPT | $SSH " 
-cat > /tmp/$MERGE_FILE_SCRIPT
-chmod u+x /tmp/$MERGE_FILE_SCRIPT
-
-# .config files
-/tmp/$MERGE_FILE_SCRIPT $UP_CONF_DIR $CONF_DIR
-# .local/share files
-/tmp/$MERGE_FILE_SCRIPT $UP_SHARE_DIR $SHARE_DIR
-# ressources files
-/tmp/$MERGE_FILE_SCRIPT $UP_RESSOURCES $CLASSES
-"
-    [ $? = 0 ] || exit_fail
-) | tee -a $LOG_FILE
-
-
-##
-## Launching the server.
-##
-log_date "Starting the bloatit server." $LOG_FILE
-(
-    $SSH "/etc/init.d/bloatit start"
-    [ $? = 0 ] || exit_fail
-) | tee -a $LOG_FILE
+startBloatitServer "$LOG_FILE" "$SSH"
 
 echo "Release done ! "
--- deployment/mergeFiles.sh
+++ deployment/mergeFiles.sh
@@ -6,32 +6,32 @@ if [ ! -e "$1" ] || [ ! -e "$2" ] ; then
 fi
 
 performMerge() {
-echo "$1 and $2 have differences. What do you want to do ?"
-echo "1 -> keep old ($2)"
-echo "2 -> use new ($1) and erase $2"
-echo "3 -> use vimdiff"
-echo "4 -> merge the files by yourself (exit or ^D to finish)."
+echo "    $1 and $2 have differences. What do you want to do ?"
+echo "    1 -> keep old ($2)"
+echo "    2 -> use new ($1) and erase $2"
+echo "    3 -> use vimdiff"
+echo "    4 -> merge the files by yourself (exit or ^D to finish)."
 read _reponse
 case $_reponse in
      1)
-         echo "keeping $2. Do nothing."
+         echo "    keeping $2. Do nothing."
          ;;
      2)
-	 echo "cp $1 $2"
+	 echo "    cp $1 $2"
 	 cp "$1" "$2"
          ;;
      3)
 	 # TODO use modification time to know if merge done.
 	 vimdiff "$1" "$2"
-	 echo "I'm assuming you have made the merge."
+	 echo "    I'm assuming you have made the merge."
          ;;
      4)
 	 # TODO use modification time to know if merge done.
-	 echo "You can use $2.new"
+	 echo "    You can use $2.new"
 	 pushd "$(dirname "$2")"
 	 bash -i 
          popd
-	 echo "I'm assuming you have made the merge."
+	 echo "    I'm assuming you have made the merge."
          ;;
      ?)
          performMerge "$1" "$2"
@@ -61,24 +61,30 @@ local _old_files=( ${_new_files[@]/#$1/$2} )
 for (( i = 0; i < ${#_new_files[@]}; i++)) ; do
     local _new_file="${_new_files[$i]}"
     local _old_file="${_old_files[$i]}"
-    echo "merging [$_new_file] to [$_old_file]"
+    echo "- Merging [$_new_file] to [$_old_file]"
 
     if [ -e "$_old_file" ] ; then
 	
         if [ "$(filesEqual "$_old_file" "$_new_file")" = "true" ] ; then
-	    echo "No modification in: $_old_file. Do nothing"
+	    echo "    No modification in: $_old_file. Do nothing"
 	elif [ "$(filesEqual "$_old_file.new" "$_new_file")" = "true" ] ; then
-	    echo "No modification since prevous transfer. Do nothing"
+	    echo "    No modification since prevous transfer. Do nothing"
 	else
 	    cp "$_new_file" "$_old_file.new"
 	    performMerge "$_new_file" "$_old_file" 
 	fi
     else
-        echo "New file $_new_file is copied to $_old_file."
+        echo "    New file $_new_file is copied to $_old_file."
 	if [ ! -e "$(dirname "$_old_file")" ] ; then
 	    mkdir -p "$(dirname "$_old_file")"
 	fi
 	cp "$_new_file" "$_old_file" 
+        echo "    Customize ? (y/N)"
+        read rep
+        if [ "$rep" = "y" ] || [ "$rep" = "Y" ] ; then
+             vim "$_old_file"
+	     cp "$_new_file" "$_old_file.new"
+        fi
     fi
 done
 }
