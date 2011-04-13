4d7661595ec9d8dae0bd37919536a67760b97d74
Frédéric Bertolus <fred@linkeos.com>
2011-04-13 11:22:04 +0200

    improve markdown style

--- main/src/main/java/com/bloatit/framework/webprocessor/components/renderer/HtmlMarkdownRenderer.java
+++ main/src/main/java/com/bloatit/framework/webprocessor/components/renderer/HtmlMarkdownRenderer.java
@@ -3,7 +3,7 @@ package com.bloatit.framework.webprocessor.components.renderer;
 import com.bloatit.framework.exceptions.highlevel.BadProgrammerException;
 import com.bloatit.framework.utils.parsers.MarkdownParser;
 import com.bloatit.framework.utils.parsers.ParsingException;
-import com.bloatit.framework.webprocessor.components.meta.HtmlLeaf;
+import com.bloatit.framework.webprocessor.components.HtmlDiv;
 import com.bloatit.framework.webprocessor.components.meta.XmlText;
 
 /**
@@ -11,19 +11,19 @@ import com.bloatit.framework.webprocessor.components.meta.XmlText;
  * A class used to represent Markdown text in html format
  * </p>
  */
-public class HtmlMarkdownRenderer extends HtmlLeaf {
+public class HtmlMarkdownRenderer extends HtmlDiv {
     private final MarkdownParser parser;
     private String renderered;
 
     /**
      * Creates a new MarkdownRenderer based on markdown or html text
-     * 
+     *
      * @param text the content to display, must be markdown text if
      *            <code>alreadyRenderer</code> is <code>true</code> or html text
      *            if <code>alreadyRenderer</code> is <code>false</code>
      */
     public HtmlMarkdownRenderer(final String text) {
-        super();
+        super("markdown_block");
         parser = new MarkdownParser();
         try {
             renderered = parser.parse(text);
