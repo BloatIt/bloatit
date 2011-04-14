562b37f9ac8ad06d1d1b559ef3130a504abaa822
Frédéric Bertolus <fred@linkeos.com>
2011-04-15 00:24:15 +0200

    fix formatting

--- main/src/main/java/com/bloatit/BloatitExampleDB.java
+++ main/src/main/java/com/bloatit/BloatitExampleDB.java
@@ -397,7 +397,6 @@ public class BloatitExampleDB {
     // feature;
     // featureImpl.getDao().setFeatureState(FeatureState.DISCARDED);
     // }
-
     public void giveMoney(final Member member, final int amount) {
         final BankTransaction bankTransaction = new BankTransaction("money !!!",
                                                                     UUID.randomUUID().toString(),
@@ -428,5 +427,4 @@ public class BloatitExampleDB {
         System.out.println("Database generation ended");
         System.exit(0);
     }
-
 }
