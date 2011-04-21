e1b82334e64b15ad6fda65ba7288de97721697f4
Thomas Guyard <thomas@linkeos.com>
2011-04-21 16:21:02 +0200

    Add a right manager for the team

---
+++ main/src/main/java/com/bloatit/model/right/TeamRight.java
@@ -0,0 +1,41 @@
+//
+// Copyright (c) 2011 Linkeos.
+//
+// This file is part of Elveos.org.
+// Elveos.org is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+//
+// Elveos.org is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+// more details.
+// You should have received a copy of the GNU General Public License along
+// with Elveos.org. If not, see http://www.gnu.org/licenses/.
+//
+package com.bloatit.model.right;
+
+import com.bloatit.model.Member;
+
+/**
+ * The Class MemberRight store the properties accessor for the {@link Member}
+ * class.
+ */
+public class TeamRight extends RightManager {
+    
+    /**
+     * The Class Email is a {@link Public} accessor for the Contact property.
+     */
+    public static class Contact extends Public {
+        // nothing this is just a rename.
+    }
+    
+    /**
+     * The Class Email is a {@link Public} accessor for the DisplayName property.
+     */
+    public static class DisplayName extends Public {
+        // nothing this is just a rename.
+    }
+
+}
