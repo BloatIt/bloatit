7544a63cd80e00a85ff48a36df7453b93af2baa5
Frédéric Bertolus <fred@linkeos.com>
2011-04-27 17:16:19 +0200

    optimize markdown cache

--- main/src/main/java/com/bloatit/framework/webprocessor/components/renderer/HtmlCachedMarkdownRenderer.java
+++ main/src/main/java/com/bloatit/framework/webprocessor/components/renderer/HtmlCachedMarkdownRenderer.java
@@ -8,21 +8,19 @@ import com.bloatit.framework.webprocessor.components.HtmlDiv;
 import com.bloatit.framework.webprocessor.components.meta.XmlText;
 
 public class HtmlCachedMarkdownRenderer extends HtmlDiv {
-    private final MarkdownParser parser;
-
     /**
      * Creates a new MarkdownRenderer based on markdown or html text
-     * 
+     *
      * @param text the content to display, must be markdown text if
      *            <code>alreadyRenderer</code> is <code>true</code> or html text
      *            if <code>alreadyRenderer</code> is <code>false</code>
      */
     public HtmlCachedMarkdownRenderer(final String text) {
         super("markdown_block");
-        parser = new MarkdownParser();
 
         final String cached = MemoryCache.getInstance().get(text);
         if (cached == null) {
+            final MarkdownParser parser = new MarkdownParser();
             try {
                 final String renderered = parser.parse(text);
                 MemoryCache.getInstance().cache(text, renderered);
