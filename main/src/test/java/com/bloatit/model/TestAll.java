495a41dd083da2918596356aff6bb96eace1bc87
Thomas Guyard <thomas@linkeos.com>
2011-05-02 13:47:38 +0200

    Make sure the tests works wen launching all of them consecutively

--- main/src/test/java/com/bloatit/model/TestAll.java
+++ main/src/test/java/com/bloatit/model/TestAll.java
@@ -6,6 +6,8 @@ package com.bloatit.model;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+import com.bloatit.data.SessionManager;
+
 /**
  * The class <code>TestAll</code> builds a suite that can be used to run all of
  * the tests within its package as well as within any subpackages of its
@@ -26,6 +28,9 @@ public class TestAll {
      */
     public static Test suite() {
         TestSuite suite;
+        
+        SessionManager.getSessionFactory().close();
+        SessionManager.generateTestSessionFactory();
 
         suite = new TestSuite("Tests in package com.bloatit.model");
         suite.addTestSuite(KudosableTest.class);
