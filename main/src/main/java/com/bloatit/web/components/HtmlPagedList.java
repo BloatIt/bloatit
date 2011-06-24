ba7e5aaf5c3692ff29df42f34d81433f552500f5
Yoann Plénet <yoann@linkeos.com>
2011-06-24 11:58:55 +0200

    Adding rel="next" and rel="prev" to HtmlPagedList

--- main/src/main/java/com/bloatit/web/components/HtmlPagedList.java
+++ main/src/main/java/com/bloatit/web/components/HtmlPagedList.java
@@ -18,6 +18,7 @@ import com.bloatit.framework.webprocessor.annotations.Optional;
 import com.bloatit.framework.webprocessor.annotations.ParamContainer;
 import com.bloatit.framework.webprocessor.annotations.RequestParam;
 import com.bloatit.framework.webprocessor.components.HtmlDiv;
+import com.bloatit.framework.webprocessor.components.HtmlLink;
 import com.bloatit.framework.webprocessor.components.HtmlList;
 import com.bloatit.framework.webprocessor.components.HtmlRenderer;
 import com.bloatit.framework.webprocessor.components.HtmlSpan;
@@ -29,7 +30,6 @@ import com.bloatit.web.url.HtmlPagedListUrlComponent;
 
 @ParamContainer(value = "pagedList", isComponent = true)
 public class HtmlPagedList<T> extends HtmlDiv {
-
     private static final int NB_PAGES_RIGHT = 2;
     private static final int NB_PAGES_TOTAL = 12;
     private static final int NB_PAGES_LEFT = 2;
@@ -55,8 +55,12 @@ public class HtmlPagedList<T> extends HtmlDiv {
         this(itemRenderer, itemList, url2, url, new PlaceHolderElement(), new PlaceHolderElement());
     }
 
-
-    public HtmlPagedList(final HtmlRenderer<T> itemRenderer, final PageIterable<T> itemList, final Url url2, final HtmlPagedListUrlComponent url, final XmlNode preListElement, final XmlNode postListElement) {
+    public HtmlPagedList(final HtmlRenderer<T> itemRenderer,
+                         final PageIterable<T> itemList,
+                         final Url url2,
+                         final HtmlPagedListUrlComponent url,
+                         final XmlNode preListElement,
+                         final XmlNode postListElement) {
 
         super("paged_list");
         this.currentPage = url.getCurrentPage();
@@ -89,87 +93,89 @@ public class HtmlPagedList<T> extends HtmlDiv {
     private HtmlElement generateLinksBar() {
         final HtmlSpan span = new HtmlSpan("pages_list");
         if (currentPage > 1) {
-            span.add(generateLink(currentPage - 1, tr("Previous"), true));
+            span.add(generateLink(currentPage - 1, tr("Previous"), true, "prev"));
         } else {
             span.add(generateEmptyLongBlock());
         }
 
-        if(pageCount <= NB_PAGES_TOTAL) {
-            //Display all pages
-            for (int i = 1; i < pageCount+1; i++) {
+        if (pageCount <= NB_PAGES_TOTAL) {
+            // Display all pages
+            for (int i = 1; i < pageCount + 1; i++) {
                 span.add(generateLink(i, false));
             }
         } else {
             // 3 cases:
-            //  - the current page is near to the first   1  2  3  4  5 -6- 7  8  ...   32 33 34
-            //  - the current page is near to the last    1  2  3 ...  27 28 29 -30- 31 32 33 34
-            //  - the current page is isolated            1  2  3 ...  9 10 -11- 12 ... 32 33 34
+            // - the current page is near to the first 1 2 3 4 5 -6- 7 8 ... 32
+            // 33 34
+            // - the current page is near to the last 1 2 3 ... 27 28 29 -30- 31
+            // 32 33 34
+            // - the current page is isolated 1 2 3 ... 9 10 -11- 12 ... 32 33
+            // 34
 
             final int slotForCenter = NB_PAGES_TOTAL - NB_PAGES_RIGHT - NB_PAGES_LEFT;
 
+            final int maxSlotForLeft = currentPage - 1 - NB_PAGES_LEFT;
+            final int maxSlotForRight = pageCount - currentPage - NB_PAGES_RIGHT;
 
-            final int maxSlotForLeft =  currentPage - 1 - NB_PAGES_LEFT;
-            final int maxSlotForRight =  pageCount - currentPage - NB_PAGES_RIGHT;
+            int slotForLeft = maxSlotForLeft;
+            int slotForRight = maxSlotForRight;
 
-            int slotForLeft =  maxSlotForLeft;
-            int slotForRight =  maxSlotForRight;
-
-            if(slotForLeft <= (slotForCenter-1)/2) {
-                slotForRight = slotForCenter - 1  - slotForLeft;
-            } else if (slotForRight <= (slotForCenter-1)/2) {
-                slotForLeft = slotForCenter - 1  - slotForRight;
+            if (slotForLeft <= (slotForCenter - 1) / 2) {
+                slotForRight = slotForCenter - 1 - slotForLeft;
+            } else if (slotForRight <= (slotForCenter - 1) / 2) {
+                slotForLeft = slotForCenter - 1 - slotForRight;
             } else {
-                if(slotForLeft > (slotForCenter-1)/2) {
-                    slotForLeft = (slotForCenter-1)/2;
+                if (slotForLeft > (slotForCenter - 1) / 2) {
+                    slotForLeft = (slotForCenter - 1) / 2;
                 }
-                slotForRight = slotForCenter - 1  - slotForLeft;
+                slotForRight = slotForCenter - 1 - slotForLeft;
             }
 
             boolean needLeftSpacer = true;
             boolean needRightSpacer = true;
-            if(currentPage - slotForLeft -1 <= NB_PAGES_LEFT) {
+            if (currentPage - slotForLeft - 1 <= NB_PAGES_LEFT) {
                 needLeftSpacer = false;
             }
 
-            if(currentPage + slotForRight   >=  pageCount - NB_PAGES_RIGHT) {
+            if (currentPage + slotForRight >= pageCount - NB_PAGES_RIGHT) {
                 needRightSpacer = false;
             }
 
-            if(needLeftSpacer) {
+            if (needLeftSpacer) {
                 slotForLeft--;
             }
 
-            if(needRightSpacer) {
+            if (needRightSpacer) {
                 slotForRight--;
             }
 
-            //Begin
-            for (int i = 1; i < NB_PAGES_LEFT +1 ; i++) {
+            // Begin
+            for (int i = 1; i < NB_PAGES_LEFT + 1; i++) {
                 span.add(generateLink(i, false));
             }
 
-            //Begin spacer
-            if(needLeftSpacer) {
+            // Begin spacer
+            if (needLeftSpacer) {
                 span.add(generateShortBlock("…"));
             }
 
-            for (int i = currentPage - slotForLeft; i < currentPage  + slotForRight+1 ; i++) {
+            for (int i = currentPage - slotForLeft; i < currentPage + slotForRight + 1; i++) {
                 span.add(generateLink(i, false));
             }
 
-          //End spacer
-            if(needRightSpacer) {
+            // End spacer
+            if (needRightSpacer) {
                 span.add(generateShortBlock("…"));
             }
 
-          //End
-            for (int i = pageCount-NB_PAGES_RIGHT+1; i < pageCount+1 ; i++) {
+            // End
+            for (int i = pageCount - NB_PAGES_RIGHT + 1; i < pageCount + 1; i++) {
                 span.add(generateLink(i, false));
             }
         }
 
         if (currentPage < pageCount) {
-            span.add(generateLink(currentPage + 1, tr("Next"), true));
+            span.add(generateLink(currentPage + 1, tr("Next"), true, "next"));
         } else {
             span.add(generateEmptyLongBlock());
         }
@@ -179,22 +185,26 @@ public class HtmlPagedList<T> extends HtmlDiv {
 
     private XmlNode generateLink(final int i, final boolean longBlock) {
         final String iString = Integer.valueOf(i).toString();
-        return generateLink(i, iString, longBlock);
+        return generateLink(i, iString, longBlock, null);
     }
 
-    private XmlNode generateLink(final int page, final String text, final boolean longBlock) {
+    private XmlNode generateLink(final int page, final String text, final boolean longBlock, String rel) {
         final String iString = Integer.valueOf(page).toString();
         final String css = (longBlock ? "long_block" : "short_block");
         if (page != currentPage) {
-
             url.setCurrentPage(page);
             url.setPageSize(pageSize);
 
-            return currentUrl.getHtmlLink(text).setCssClass(css);
+            HtmlLink htmlLink = currentUrl.getHtmlLink(text);
+            htmlLink.setCssClass(css);
+
+            if (rel != null) {
+                htmlLink.addAttribute("rel", rel);
+            }
+
+            return htmlLink;
         }
-
-
-        return new HtmlSpan(css+"_active").addText(iString);
+        return new HtmlSpan(css + "_active").addText(iString);
     }
 
     private XmlNode generateEmptyLongBlock() {
