06bcedd94fa53e7ad65609ae0b76df5d344885ca
Frédéric Bertolus <fred@linkeos.com>
2011-06-30 23:42:39 +0200

    update button

--- www/resources/commons/api/button.js
+++ www/resources/commons/api/button.js
@@ -25,20 +25,57 @@ function elveos_ajax(url, callback) {
 }
 
 function elveos_startGenerateButton(button) {
-    featureId = button.getAttribute('data-feature-id');
+    var featureId = button.getAttribute('data-feature-id');
+    var buttonStyle = button.getAttribute('data-button-style');
     
     //var host = 'https://elveos.org'
     //var host = 'http://f2.b219.org:8081'
-   var host = 'http://'+ window.location.hostname;    
+    var host = 'http://'+ window.location.hostname;    
     elveos_ajax(host + '/rest/features/'+featureId, function(xml) {
     contribution = parseFloat(xml.getElementsByTagName('contribution')[0].firstChild.data);
+    progression = parseFloat(xml.getElementsByTagName('feature')[0].getAttribute("progression"));
     button.innerHTML = "" + contribution + " €";
     button.style.display = "inline-block";
-    button.style.height = "18px";
-    button.style.width = "50px";
-    button.style.paddingLeft = "20px";
-    button.style.border = "1px solid rgb(0,82,108)";
-    button.style.background = "white";
+    button.style.textAlign = "center";
+    button.style.font= "bold 12px/20px Arial,Sans";
+    button.style.color= "black";
+    
+    
+    if(buttonStyle == "short_small") {
+        button.style.lineHeight= "20px";
+        button.style.height = "20px";
+        button.style.width = "50px";
+        button.style.paddingLeft = "27px";
+    } else if (buttonStyle == "short_medium") {
+        button.style.height = "24px";
+        button.style.width = "93px";
+        //button.style.paddingLeft = "20px";
+    } else if (buttonStyle == "short_big") {
+        button.style.height = "32px";
+        button.style.width = "124px";
+        //button.style.paddingLeft = "20px";
+    } else if (buttonStyle == "long_small") {
+        button.style.height = "20px";
+        button.style.width = "49px";
+        button.style.paddingLeft = "74px";
+    } else if (buttonStyle == "long_medium") {
+        button.style.height = "24px";
+        button.style.width = "160px";
+        //button.style.paddingLeft = "20px";
+    } else if (buttonStyle == "long_big") {
+        button.style.height = "32px";
+        button.style.width = "214px";
+        //button.style.paddingLeft = "20px";
+    }
+    
+    var progress = parseInt(progression);
+    progress = progress/10;
+    progress = progress*10;
+    if(progress > 100) {
+        progress = 100;
+    }
+    
+    button.style.background = "url(\""+host+"/resources/commons/api/button_"+buttonStyle+"_"+progress+".png\") no-repeat scroll 0 0 transparent";
     }
     );
 }
